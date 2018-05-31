4963358690e9a1a713bba2dc25a2a996c22b93ea
thomas-95is <thomas.stokes@informed.com>
2018-05-31 15:36:14 +0100

    urls.py: Add configuration for 'Service not available' view. Remove project template views.

--- login_app/urls.py
+++ login_app/urls.py
@@ -5,11 +5,12 @@ from login_app import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^simple_form/', views.simple_form, name='simple_form'),
-    url(r'^long_form/', views.long_form, name='long_form'),
-    url(r'^fieldset_form/', views.fieldset_form, name='fieldset_form'),
-    url(r'^revealing_form/', views.revealing_form, name='revealing_form')
+    # url(r'^$', views.index, name='index'),
+    # url(r'^simple_form/', views.simple_form, name='simple_form'),
+    # url(r'^long_form/', views.long_form, name='long_form'),
+    # url(r'^fieldset_form/', views.fieldset_form, name='fieldset_form'),
+    # url(r'^revealing_form/', views.revealing_form, name='revealing_form')
+    url(r'service-unavailable/', views.ServiceUnavailableView.as_view(), name="Service-Unavailable"),
 ]
 
 # Django toolbar settings for development environments
