1c522607f059837ec1b2db607924027f63765384
thomas-95is <thomas.stokes@informed.com>
2018-05-31 15:49:02 +0100

    tests.py: Add test to assert 'Service not available' page can be rendered.

--- login_app/tests/tests_views/tests.py
+++ login_app/tests/tests_views/tests.py
@@ -2,13 +2,12 @@ from django.test import TestCase
 from django.urls import reverse
 
 
-class ExampleTests(TestCase):
+class LoginTests(TestCase):
 
-    def test_can_render_template_page(self):
+    def test_can_render_service_unavailable(self):
         """
-        A simple example test to assert the index page for the template can be rendered
-        :return:
+        A simple example test to assert the 'service unavailable' page can be rendered
         """
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('Service-Unavailable'))
 
         self.assertEqual(response.status_code, 200)
