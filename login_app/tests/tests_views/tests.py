fe4551d0b9d4314796b23eac12f6137ddba1ec6a
thomas-95is <thomas.stokes@informed.com>
2018-06-01 10:17:39 +0100

    tests.py: Add tests for 'Account-Selection' page.

--- login_app/tests/tests_views/tests.py
+++ login_app/tests/tests_views/tests.py
@@ -1,13 +1,53 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+
+from login_app import views
 
 
 class LoginTests(TestCase):
 
     def test_can_render_service_unavailable(self):
         """
-        A simple example test to assert the 'service unavailable' page can be rendered
+        Test to assert that the 'service unavailable' page can be rendered.
         """
         response = self.client.get(reverse('Service-Unavailable'))
 
         self.assertEqual(response.status_code, 200)
+
+    def test_can_render_accout_selection_page(self):
+        """
+        Test to assert that the 'Account-Selection' page can be rendered.
+        """
+        response = self.client.get(reverse('Account-Selection'))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_empty_account_selection_form_redirects_to_self(self):
+        """
+        Test to assert that not selecting an option on the 'Account-Selection' redirects back to the same page.
+        """
+        response = self.client.post(reverse('Account-Selection'), {'account_selection': None})
+        found = resolve(response.request.get('PATH_INFO'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(found.func.view_class, views.AccountSelectionFormView)
+
+    def test_can_select_new_application(self):
+        """
+        Test to assert that selecting 'Return to application' on the 'Account-Selection' page redirects to appropriate
+        page.
+        """
+        response = self.client.post(reverse('Account-Selection'), {'account_selection': 'new'})
+
+        self.assertEqual(response.status_code, 302)
+        # TODO : Assert exactly which page the client should land on.
+
+    def test_can_select_existing_application(self):
+        """
+        Test to assert that selecting 'Return to application' on the 'Account-Selection' page redirects to appropriate
+        page.
+        """
+        response = self.client.post(reverse('Account-Selection'), {'account_selection': 'existing'})
+
+        self.assertEqual(response.status_code, 302)
+        # TODO : Assert exactly which page the client should land on.
