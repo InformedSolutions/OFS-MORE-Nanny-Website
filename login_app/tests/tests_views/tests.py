ff6d545b05f0547f2aab34ffd285d980b6ff517d
thomas-95is <thomas.stokes@informed.com>
2018-06-01 12:18:01 +0100

    tests.py: Add tests for 'New-User-Sign-In' page.

--- login_app/tests/tests_views/tests.py
+++ login_app/tests/tests_views/tests.py
@@ -38,9 +38,11 @@ class LoginTests(TestCase):
         page.
         """
         response = self.client.post(reverse('Account-Selection'), {'account_selection': 'new'})
+        # found = resolve(response.request.get('url'))
+        found = resolve(response.url)
 
         self.assertEqual(response.status_code, 302)
-        # TODO : Assert exactly which page the client should land on.
+        self.assertEqual(found.func.view_class, views.NewUserSignInFormView)
 
     def test_can_select_existing_application(self):
         """
@@ -51,3 +53,42 @@ class LoginTests(TestCase):
 
         self.assertEqual(response.status_code, 302)
         # TODO : Assert exactly which page the client should land on.
+
+    def test_can_render_new_user_sign_in_page(self):
+        """
+        Test to assert that the 'New-User-Sign-In' page can be rendered.
+        """
+        response = self.client.get(reverse('New-User-Sign-In'))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_email_redirects_to_self(self):
+        """
+        Test that entering an invalid email on the 'New-User-Sign-In' page redirects back to the same page.
+        """
+        test_invalid_emails = (
+            '',
+            't',
+            '123',
+            'knights@ni'
+        )
+
+        for email in test_invalid_emails:
+            response = self.client.post(reverse('New-User-Sign-In'), {'email_address': email})
+            found = resolve(response.url)
+
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(found.func.view_class, views.NewUserSignInFormView)
+
+    def test_valid_email_address_creates_account_and_redirects(self):
+        """
+        Test that entering a valid email address on the 'New-User-Sign-In' page creates an account with that email and
+        redirects to the appropriate page.
+        """
+        response = self.client.post(reverse('New-User-Sign-In'), {'email_address': 'eva@walle.com'})
+        found = resolve(response.request.get('PATH_INFO'))
+
+        self.assertEqual(response.status_code, 302)
+        # self.assertEqual(found.func.view_class, views.CheckEmailView)
+
+        #TODO - Assert response codes from Identity API for queries before and after POST request with valid email.
