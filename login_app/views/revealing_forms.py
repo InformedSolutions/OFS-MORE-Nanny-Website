341bd921653c67b2a3e1ce47744dd58f7266fc68
thomas-95is <thomas.stokes@informed.com>
2018-06-01 17:19:05 +0100

    Remove template files.

--- login_app/views/revealing_forms.py
+++
@@ -1,7 +0,0 @@
-from login_app.views import render_form
-from login_app.forms.forms import RevealingForm
-
-
-def revealing_form(request):
-    form = RevealingForm()
-    return render_form(request, form)
