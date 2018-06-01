96843da9b39f4e47e7e1985bd8d8f55c76c2d811
thomas-95is <thomas.stokes@informed.com>
2018-06-01 16:11:22 +0100

    existing_user_sign_in.py: Add view for 'Existing-User-Sign-In' page.

---
+++ login_app/views/existing_user_sign_in.py
@@ -0,0 +1,21 @@
+from django.views.generic import FormView
+from django.shortcuts import reverse
+from django.http import HttpResponseRedirect
+
+from login_app.forms import ContactEmailForm
+
+
+class ExistingUserSignIn(FormView):
+    """
+    Class containing the methods for handling requests to the 'New-User-Sign-In' page.
+    """
+    template_name = 'existing-user-sign-in.html'
+    form_class = ContactEmailForm
+    success_url = 'Check-Email'
+
+    def form_valid(self, request):
+        # TODO - Insert calls to Identity-Gateway API for creation of new application.
+        # TODO - Add calls to Notify-Gateway API to send email to appplicant, once above implemented.
+
+        # return HttpResponseRedirect(self.get_success_url())
+        return HttpResponseRedirect(reverse(self.success_url))
