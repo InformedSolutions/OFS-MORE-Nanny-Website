f2e406fa08bfc2ec23fd75d7bda06126206591a5
thomas-95is <thomas.stokes@informed.com>
2018-06-01 15:34:47 +0100

    service_unavailable.py: Add class-level comment.

--- login_app/views/service_unavailable.py
+++ login_app/views/service_unavailable.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 
 
 class ServiceUnavailableView(View):
+    """
+    Class containing the view(s) for handling the GET requests to the 'Service-Unavailable' page.
+    """
     def get(self, request):
         return render(request, 'service-unavailable.html')
