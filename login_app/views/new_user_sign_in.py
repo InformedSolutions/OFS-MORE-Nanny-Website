2f1fb1d825cd966e42e8fe57522f5ef126b9eb57
thomas-95is <thomas.stokes@informed.com>
2018-06-06 17:33:08 +0100

    new_user_sign_in.py: Use base class get_success_parameters method instead.

--- login_app/views/new_user_sign_in.py
+++ login_app/views/new_user_sign_in.py
@@ -26,12 +26,3 @@ class NewUserSignInFormView(BaseFormView):
         # TODO - Add calls to Notify-Gateway API to send email to applicant.
 
         return HttpResponseRedirect(self.get_success_url())
-
-    def get_success_parameters(self):
-        """
-        Method to return a dictionary of parameters to be included as variables in the success url, e.g. application_id.
-        """
-        params = dict()
-        params['email_address'] = self.request.POST['email_address']
-
-        return params
