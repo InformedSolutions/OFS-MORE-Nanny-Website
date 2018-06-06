1e19e6473442f8724cec0361f3f5add14fa503d9
thomas-95is <thomas.stokes@informed.com>
2018-06-06 11:33:02 +0100

    utils.py: Add build_url function to utils.py

--- login_app/utils.py
+++ login_app/utils.py
@@ -1,6 +1,17 @@
 import re
 
+from urllib.parse import urlencode
+
 from django import forms
+from django.shortcuts import reverse
+
+
+def build_url(*args, **kwargs):
+    get = kwargs.pop('get', {})
+    url = reverse(*args, **kwargs)
+    if get:
+        url += '?' + urlencode(get)
+    return url
 
 
 class PhoneNumberField(forms.CharField):
