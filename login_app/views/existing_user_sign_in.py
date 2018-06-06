033e6f386444dbd86ce86c52846a7790c54201e7
thomas-95is <thomas.stokes@informed.com>
2018-06-06 11:35:43 +0100

    Update existing_user_sign_in and new_user_sign_in to use new BaseFormView.

--- login_app/views/existing_user_sign_in.py
+++ login_app/views/existing_user_sign_in.py
@@ -1,11 +1,14 @@
-from django.views.generic import FormView
-from django.shortcuts import reverse
+import uuid
+
 from django.http import HttpResponseRedirect
 
+from identity_models.user_details import UserDetails
+
 from login_app.forms import ContactEmailForm
+from .base import BaseFormView
 
 
-class ExistingUserSignInFormView(FormView):
+class ExistingUserSignInFormView(BaseFormView):
     """
     Class containing the methods for handling requests to the 'New-User-Sign-In' page.
     """
@@ -13,9 +16,22 @@ class ExistingUserSignInFormView(FormView):
     form_class = ContactEmailForm
     success_url = 'Check-Existing-Email'
 
-    def form_valid(self, request):
-        # TODO - Insert calls to Identity-Gateway API for creation of new application.
-        # TODO - Add calls to Notify-Gateway API to send email to appplicant, once above implemented.
+    def form_valid(self, email_form):
+        email_address = email_form.cleaned_data['email_address']
+        record = UserDetails.api.get_record(email=email_address)
 
-        # return HttpResponseRedirect(self.get_success_url())
-        return HttpResponseRedirect(reverse(self.success_url))
+        if record.status_code == 404:
+            UserDetails.api.create(email=email_address, application_id=uuid.uuid4())
+
+        # TODO - Add calls to Notify-Gateway API to send email to applicant.
+
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_parameters(self):
+        """
+        Method to return a dictionary of parameters to be included as variables in the success url, e.g. application_id.
+        """
+        params = dict()
+        params['email_address'] = self.request.POST['email_address']
+
+        return params
--- login_app/views/new_user_sign_in.py
+++ login_app/views/new_user_sign_in.py
@@ -1,15 +1,14 @@
 import uuid
 
-from django.views.generic import FormView
-from django.shortcuts import reverse
 from django.http import HttpResponseRedirect
-
-from login_app.forms import ContactEmailForm
 
 from identity_models.user_details import UserDetails
 
+from login_app.forms import ContactEmailForm
+from .base import BaseFormView
 
-class NewUserSignInFormView(FormView):
+
+class NewUserSignInFormView(BaseFormView):
     """
     Class containing the methods for handling requests to the 'New-User-Sign-In' page.
     """
@@ -17,13 +16,22 @@ class NewUserSignInFormView(FormView):
     form_class = ContactEmailForm
     success_url = 'Check-New-Email'
 
-    def form_valid(self, request):
-        email_address = request.cleaned_data['email_address']
+    def form_valid(self, email_form):
+        email_address = email_form.cleaned_data['email_address']
         record = UserDetails.api.get_record(email=email_address)
 
         if record.status_code == 404:
             UserDetails.api.create(email=email_address, application_id=uuid.uuid4())
 
-        # TODO - Add calls to Notify-Gateway API to send email to appplicant, once above implemented.
+        # TODO - Add calls to Notify-Gateway API to send email to applicant.
 
-        return HttpResponseRedirect(reverse(self.success_url))
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_parameters(self):
+        """
+        Method to return a dictionary of parameters to be included as variables in the success url, e.g. application_id.
+        """
+        params = dict()
+        params['email_address'] = self.request.POST['email_address']
+
+        return params
