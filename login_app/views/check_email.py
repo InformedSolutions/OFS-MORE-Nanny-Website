c5fc93eda2151edfae1793fcce535640603924e4
thomas-95is <thomas.stokes@informed.com>
2018-06-06 11:36:13 +0100

    check_email.py: Grab email_address encoded within url.

--- login_app/views/check_email.py
+++ login_app/views/check_email.py
@@ -7,6 +7,5 @@ class CheckEmailView(View):
     Class containing the view(s) for handling the GET requests to the 'Check-Email' page.
     """
     def get(self, request):
-        # email_address = request.GET['email_address']
-        email_address = 'placeholder_email@add_api.com'  # Have placeholder email until Identify API operational.
+        email_address = request.GET['email_address']
         return render(request, 'check-email.html', context={'email_address': email_address})
