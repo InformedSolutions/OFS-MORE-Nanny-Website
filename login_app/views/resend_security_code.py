4c6a11ddbb56dd3a6cc0ea29d7fea2a274d5c6ae
thomas-95is <thomas.stokes@informed.com>
2018-06-07 16:23:44 +0100

    resend_security_code.py: Add logic to reset account security code.

--- login_app/views/resend_security_code.py
+++ login_app/views/resend_security_code.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, reverse
+from django.shortcuts import render
 from django.views import View
+
+from identity_models.user_details import UserDetails
+
+from login_app.utils import build_url
+
+from .validate_magic_link import ValidateMagicLinkView
 
 
 class ResendSecurityCodeView(View):
@@ -11,4 +17,10 @@ class ResendSecurityCodeView(View):
         return render(request, template_name='resend-security-code.html')
 
     def post(self, request):
-        return HttpResponseRedirect(reverse('Security-Code'))
+        get = {'email_address': request.GET['email_address']}
+
+        record = UserDetails.api.get_record(email=get['email_address']).record
+        record = ValidateMagicLinkView.sms_magic_link(record=record)
+        UserDetails.api.put(record)
+
+        return HttpResponseRedirect(build_url('Security-Code', get=get))
