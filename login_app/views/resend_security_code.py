b3297fd1c4277b9b92dea5263816b98314a9e417
thomas-95is <thomas.stokes@informed.com>
2018-06-07 18:07:50 +0100

    resend_security_code.py: Add logic for checking number of resend attempts by applicant.

--- login_app/views/resend_security_code.py
+++ login_app/views/resend_security_code.py
@@ -14,6 +14,13 @@ class ResendSecurityCodeView(View):
     Class handling requests to 'Resend-Security-Code' page.
     """
     def get(self, request):
+        email_address = request.GET['email_address']
+        record = UserDetails.api.get_record(email=email_address).record
+
+        # If the applicant has attempted more than 3 attempts in the past 24 hours, redirect to security question.
+        if record['sms_resend_attempts'] >= 3:
+            return HttpResponseRedirect(build_url('Security-Question', get={'email_address': email_address}))
+
         return render(request, template_name='resend-security-code.html')
 
     def post(self, request):
@@ -21,6 +28,12 @@ class ResendSecurityCodeView(View):
 
         record = UserDetails.api.get_record(email=get['email_address']).record
         record = ValidateMagicLinkView.sms_magic_link(record=record)
+
+        if record['sms_resend_attempts'] is not None:
+            record['sms_resend_attempts'] += 1
+        else:
+            record['sms_resend_attempts'] = 1
+
         UserDetails.api.put(record)
 
         return HttpResponseRedirect(build_url('Security-Code', get=get))
