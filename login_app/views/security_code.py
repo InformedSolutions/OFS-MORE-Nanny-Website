caa735c5c8910d27780a9d0551ca00f1e461d249
thomas-95is <thomas.stokes@informed.com>
2018-06-07 16:21:39 +0100

    security_code.py: Override base class method to add context data for template.

--- login_app/views/security_code.py
+++ login_app/views/security_code.py
@@ -17,3 +17,9 @@ class SecurityCodeFormView(BaseFormView):
         kwargs = super(SecurityCodeFormView, self).get_form_kwargs()
         kwargs['correct_sms_code'] = UserDetails.api.get_record(email=self.request.GET['email_address']).record['magic_link_sms']
         return kwargs
+
+    def get_context_data(self, **kwargs):
+        kwargs = super(SecurityCodeFormView, self).get_context_data()
+        kwargs['mobile_number'] = UserDetails.api.get_record(email=self.request.GET['email_address']).record['mobile_number'][-3]
+        kwargs['email_address'] = self.request.GET['email_address']  # Pass to context for the hyperlinks.
+        return kwargs
