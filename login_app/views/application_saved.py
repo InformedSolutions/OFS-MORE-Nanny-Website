90b5fd335acca763f074d0eaa85e7b0dee881301
thomas-95is <thomas.stokes@informed.com>
2018-06-12 16:46:41 +0100

    application_saved: Destroy session upon signing out.

--- login_app/views/application_saved.py
+++ login_app/views/application_saved.py
@@ -1,10 +1,16 @@
 from django.views import View
 from django.shortcuts import render
 
+from middleware import CustomAuthenticationHandler
+
 
 class ApplicationSavedView(View):
     """
     Class containing the view(s) for handling the GET requests to the 'Application-Saved' page.
     """
     def get(self, request):
-        return render(request, 'application-saved.html')
+        cookie_key = CustomAuthenticationHandler.get_cookie_identifier()
+        request.COOKIES[cookie_key] = None
+        response = render(request, 'application-saved.html')
+        CustomAuthenticationHandler.destroy_session(response)
+        return response
