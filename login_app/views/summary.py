79f9609410be69f9058d15532f87b37862bdd0a0
thomas-95is <thomas.stokes@informed.com>
2018-06-12 12:20:36 +0100

    summary.py: Update redirect link following sumamry page.

--- login_app/views/summary.py
+++ login_app/views/summary.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 
 from identity_models.user_details import UserDetails
 
@@ -8,3 +9,6 @@ class ContactDetailsSummaryView(View):
     def get(self, request):
         user_details_record = UserDetails.api.get_record(email=request.GET['email_address']).record
         return render(request, template_name='contact-details-summary.html', context=user_details_record)
+
+    def post(self, request):
+        return HttpResponseRedirect(reverse('Task-List') + '/?email_address=' + request.GET['email_address'])
