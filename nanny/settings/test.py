56f40d3fe6d09d8662c49c65ec2f73760ce86874
Alice King <30341523+kingainformed@users.noreply.github.com>
2018-06-12 17:59:34 +0100

    remove undefined settings variable

--- nanny/settings/test.py
+++ nanny/settings/test.py
@@ -1,6 +1,6 @@
 from .base import *
 
-INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROD_APPS + PROJECT_APPS
+INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '!cpk9yyix!m9(jgc7ufreu-il0nj)p@w!l)g4cg@g8#-c9uq-p')
 
