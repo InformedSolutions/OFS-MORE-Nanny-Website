cef6e24f307915cd5a5c6b880eb19aa982bfeae3
thomas-95is <thomas.stokes@informed.com>
2018-06-12 16:50:18 +0100

    summary.py: Change POST request redirection.

--- login_app/views/summary.py
+++ login_app/views/summary.py
@@ -7,8 +7,12 @@ from identity_models.user_details import UserDetails
 
 class ContactDetailsSummaryView(View):
     def get(self, request):
-        user_details_record = UserDetails.api.get_record(email=request.GET['email_address']).record
+        # Depending if the user is coming from task-list or sign-in page.
+        try:
+            user_details_record = UserDetails.api.get_record(application_id=request.GET['id']).record
+        except:
+            user_details_record = UserDetails.api.get_record(application_id=request.GET['id']).record
         return render(request, template_name='contact-details-summary.html', context=user_details_record)
 
     def post(self, request):
-        return HttpResponseRedirect(reverse('Task-List') + '/?email_address=' + request.GET['email_address'])
+        return HttpResponseRedirect(reverse('Task-List') + '?id=' + request.GET['id'])
