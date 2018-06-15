f416132e58d762d3255fb4af5eee5490e76afe3d
Alice King <alice.king@informed.com>
2018-06-15 10:59:03 +0100

    add allowed hosts settings to test

--- nanny/settings/test.py
+++ nanny/settings/test.py
@@ -4,6 +4,8 @@ URL_PREFIX = '/nanny'
 
 STATIC_URL = URL_PREFIX + '/static/'
 
+ALLOWED_HOSTS = ['*']
+
 INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '!cpk9yyix!m9(jgc7ufreu-il0nj)p@w!l)g4cg@g8#-c9uq-p')
