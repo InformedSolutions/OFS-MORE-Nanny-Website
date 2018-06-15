9ca99227ef5f3b6f8226ed76c9f68505e8accd9b
thomas-95is <thomas.stokes@informed.com>
2018-06-15 09:10:22 +0100

    test.py: Add STATIC_URL to test settings.

--- nanny/settings/test.py
+++ nanny/settings/test.py
@@ -1,5 +1,9 @@
 from .base import *
 
+URL_PREFIX = '/nanny'
+
+STATIC_URL = URL_PREFIX + '/static/'
+
 INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '!cpk9yyix!m9(jgc7ufreu-il0nj)p@w!l)g4cg@g8#-c9uq-p')
