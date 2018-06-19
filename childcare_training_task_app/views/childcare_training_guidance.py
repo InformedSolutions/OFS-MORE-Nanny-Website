50345f335fb2f3a9257fab9f4d848f00f9048baa
Thomas-95is <thomas.stokes@informed.com>
2018-06-19 16:51:47 +0100

    Add POST request to 'Childcare-Training-Guidance' page.

--- childcare_training_task_app/views/childcare_training_guidance.py
+++ childcare_training_task_app/views/childcare_training_guidance.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, reverse
 from django.views import View
+
+
+from nanny_models.application import Application
 
 
 class ChildcareTrainingGuidanceView(View):
     def get(self, request):
         return render(request, template_name='childcare-training-guidance.html')
+
+    def post(self, request):
+        application_id = request.GET['id']
+        record = Application.api.get_record(application_id=application_id).record
+        record['childcare_training_status'] = "STARTED"
+        Application.api.put(application_id=application_id, record=record)
+        return HttpResponseRedirect(reverse('Type-Of-Childcare-Training') + '?id=' + application_id)
