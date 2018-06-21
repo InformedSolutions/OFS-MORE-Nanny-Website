4fea97957d266a22d9c2394eb69bac560ecbef8f
Thomas-95is <thomas.stokes@informed.com>
2018-06-21 11:41:07 +0100

    childcare_training_summary: Add context to GET request.

--- childcare_training_task_app/views/childcare_training_summary.py
+++ childcare_training_task_app/views/childcare_training_summary.py
@@ -3,11 +3,15 @@ from django.shortcuts import render, reverse
 from django.views import View
 
 from nanny_models.application import Application
+from nanny_models.childcare_training import ChildcareTraining
 
 
 class ChildcareTrainingSummaryView(View):
     def get(self, request):
-        return render(request, template_name='childcare-training-summary.html')
+        application_id = request.GET['id']
+        nanny_api_response = ChildcareTraining.api.get_record(application_id=application_id)
+        context = {'id': application_id, 'record': nanny_api_response.record}
+        return render(request, template_name='childcare-training-summary.html', context=context)
 
     def post(self, request):
         application_id = request.GET['id']
