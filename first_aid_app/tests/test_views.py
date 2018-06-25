286881d32b2a3bf18d9840a64e3e14a5d2fb7f03
GeevesH-IS <hugo.geeves@informed.com>
2018-06-25 18:02:46 +0100

    fix indentation of first aid tests and remove model from service

--- first_aid_app/tests/test_views.py
+++ first_aid_app/tests/test_views.py
@@ -34,22 +34,54 @@ class FirstAidTrainingTests(TestCase):
         self.assertEqual(r.status_code, 200)
 
     def test_can_access_details(self):
-        r = self.client.get(reverse('first-aid:Training-Details'), {'id': self.app_id})
-        self.assertEqual(r.status_code, 200)
-
-    def test_can_access_declaration(self):
-        r = self.client.get(reverse('first-aid:First-Aid-Declaration'), {'id': self.app_id})
-        self.assertEqual(r.status_code, 200)
-
-    def test_can_access_summary(self):
-        with mock.patch('nanny_models.nanny_application.NannyApplication.api.get_record') as nanny_api_app_get, \
+        with mock.patch('nanny_models.first_aid.FirstAidTraining.api.get_record') as nanny_api_get, \
+                mock.patch('nanny_models.first_aid.FirstAidTraining.api.create') as nanny_api_post, \
+                mock.patch('nanny_models.first_aid.FirstAidTraining.api.put') as nanny_api_put, \
+                mock.patch('nanny_models.nanny_application.NannyApplication.api.get_record') as nanny_api_app_get, \
                 mock.patch('nanny_models.nanny_application.NannyApplication.api.put') as nanny_api_app_put:
+            nanny_api_get.return_value.status_code = 200
+            nanny_api_get.return_value.record = {
+                'training_organisation': 'St Johns Ambulance',
+                'course_title': 'Pediatric First Aid',
+                'course_date': '2016-03-31'
+            }
+            nanny_api_post.return_value.status_code = 201
+            nanny_api_put.return_value.status_code = 201
             nanny_api_app_get.return_value.status_code = 200
             nanny_api_app_get.return_value.record = {
                 'application_id': '3575d19f-5bfc-4fcc-a7cf-229323876043',
                 'first_aid_training_status': 'IN_PROGRESS'
             }
             nanny_api_app_put.return_value.status_code = 201
+
+            r = self.client.get(reverse('first-aid:Training-Details'), {'id': self.app_id})
+            self.assertEqual(r.status_code, 200)
+
+    def test_can_access_declaration(self):
+        r = self.client.get(reverse('first-aid:First-Aid-Declaration'), {'id': self.app_id})
+        self.assertEqual(r.status_code, 200)
+
+    def test_can_access_summary(self):
+        with mock.patch('nanny_models.first_aid.FirstAidTraining.api.get_record') as nanny_api_get, \
+                mock.patch('nanny_models.first_aid.FirstAidTraining.api.create') as nanny_api_post, \
+                mock.patch('nanny_models.first_aid.FirstAidTraining.api.put') as nanny_api_put, \
+                mock.patch('nanny_models.nanny_application.NannyApplication.api.get_record') as nanny_api_app_get, \
+                mock.patch('nanny_models.nanny_application.NannyApplication.api.put') as nanny_api_app_put:
+            nanny_api_get.return_value.status_code = 200
+            nanny_api_get.return_value.record = {
+                'training_organisation': 'St Johns Ambulance',
+                'course_title': 'Pediatric First Aid',
+                'course_date': '2016-03-31'
+            }
+            nanny_api_post.return_value.status_code = 201
+            nanny_api_put.return_value.status_code = 201
+            nanny_api_app_get.return_value.status_code = 200
+            nanny_api_app_get.return_value.record = {
+                'application_id': '3575d19f-5bfc-4fcc-a7cf-229323876043',
+                'first_aid_training_status': 'IN_PROGRESS'
+            }
+            nanny_api_app_put.return_value.status_code = 201
+
 
             r = self.client.get(reverse('first-aid:First-Aid-Summary'), {'id': self.app_id})
             p = self.client.post(reverse('first-aid:First-Aid-Summary'), {'id': self.app_id})
@@ -90,15 +122,34 @@ class FirstAidTrainingTests(TestCase):
             self.assertEqual(r.status_code, 302)
 
     def test_invalid_course_date_stopped(self):
+        with mock.patch('nanny_models.first_aid.FirstAidTraining.api.get_record') as nanny_api_get, \
+                mock.patch('nanny_models.first_aid.FirstAidTraining.api.create') as nanny_api_post, \
+                mock.patch('nanny_models.first_aid.FirstAidTraining.api.put') as nanny_api_put, \
+                mock.patch('nanny_models.nanny_application.NannyApplication.api.get_record') as nanny_api_app_get, \
+                mock.patch('nanny_models.nanny_application.NannyApplication.api.put') as nanny_api_app_put:
+            nanny_api_get.return_value.status_code = 200
+            nanny_api_get.return_value.record = {
+                'training_organisation': 'St Johns Ambulance',
+                'course_title': 'Pediatric First Aid',
+                'course_date': '2016-03-31'
+            }
+            nanny_api_post.return_value.status_code = 201
+            nanny_api_put.return_value.status_code = 201
+            nanny_api_app_get.return_value.status_code = 200
+            nanny_api_app_get.return_value.record = {
+                'application_id': '3575d19f-5bfc-4fcc-a7cf-229323876043',
+                'first_aid_training_status': 'IN_PROGRESS'
+            }
+            nanny_api_app_put.return_value.status_code = 201
 
-        data = {
-            'id': self.app_id,
-            'first_aid_training_organisation': 'St Johns Ambulance',
-            'title_of_training_course': 'Pediatric First Aid',
-            'course_date_0': '12',
-            'course_date_1': '3',
-            'course_date_2': '2014',
-        }
+            data = {
+                'id': self.app_id,
+                'first_aid_training_organisation': 'St Johns Ambulance',
+                'title_of_training_course': 'Pediatric First Aid',
+                'course_date_0': '12',
+                'course_date_1': '3',
+                'course_date_2': '2014',
+            }
 
-        r = self.client.post(reverse('first-aid:Training-Details'), data, params={'id': self.app_id})
-        self.assertEqual(r.status_code, 200)
+            r = self.client.post(reverse('first-aid:Training-Details'), data, params={'id': self.app_id})
+            self.assertEqual(r.status_code, 200)
