0d7475cd547cc94e5f0da3143e58f3c76586636d
Thomas-95is <thomas.stokes@informed.com>
2018-06-26 12:36:19 +0100

    summary.py: Add logic for including 'change' links in contact details summary.

--- login_app/views/summary.py
+++ login_app/views/summary.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 
 from identity_models.user_details import UserDetails
 
+from nanny_models.nanny_application import NannyApplication
+
 from middleware import CustomAuthenticationHandler
 
 from login_app.utils import build_url
@@ -11,11 +13,16 @@ from login_app.utils import build_url
 
 class ContactDetailsSummaryView(View):
     def get(self, request):
-        return render(request, template_name='contact-details-summary.html', context=self.get_user_details_record(request))
+        application_id = request.GET['id']
+        context = self.get_user_details_record(application_id)
+        context['include_change_links'] = self.include_change_links(application_id)
+        context['application_id'] = application_id
+        return render(request, template_name='contact-details-summary.html', context=context)
 
     def post(self, request):
-        user_details_record = self.get_user_details_record(request)
-        response = HttpResponseRedirect(build_url('Task-List', get={'id': user_details_record['application_id']}))
+        application_id = request.GET['id']
+        user_details_record = self.get_user_details_record(application_id)
+        response = HttpResponseRedirect(build_url('Task-List', get={'id': application_id}))
 
         COOKIE_IDENTIFIER = CustomAuthenticationHandler.get_cookie_identifier()
         if COOKIE_IDENTIFIER not in request.COOKIES:
@@ -23,5 +30,11 @@ class ContactDetailsSummaryView(View):
 
         return response
 
-    def get_user_details_record(self, request):
-        return UserDetails.api.get_record(application_id=request.GET['id']).record
+    def get_user_details_record(self, application_id):
+        return UserDetails.api.get_record(application_id=application_id).record
+
+    def include_change_links(self, application_id):
+        """ If the applicant is coming from task list, an application object will exist => get_record gives 200 code."""
+        nanny_api_response = NannyApplication.api.get_record(application_id=application_id)
+        # return True if nanny_api_response.status_code == 200 else False
+        return nanny_api_response.status_code == 200
