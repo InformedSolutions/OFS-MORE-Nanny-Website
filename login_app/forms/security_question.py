38550a31b5d32758adf6d81c03b38ffe55d5145b
Thomas-95is <thomas.stokes@informed.com>
2018-06-29 16:20:24 +0100

    security_question.py: Remove base class. Create three separate forms for security question.

--- login_app/forms/security_question.py
+++ login_app/forms/security_question.py
@@ -4,52 +4,95 @@ from govuk_forms.forms import GOVUKForm
 from govuk_forms.widgets import Widget, TextInput
 
 from login_app.utils import DBSNumberField, PhoneNumberField
+from personal_details_app.customfields import CustomSplitDateFieldDOB
 
 
 class NannyFormInput(TextInput, Widget):
     input_classes = "nanny-form form-control"
 
 
-class BaseSecurityQuestionForm(GOVUKForm):
+class DBSSecurityQuestionForm(GOVUKForm):
     """
-    Base GOV.UK form from which individual security question forms will inherit.
+    GOV.UK form for the dbs number security question.
     """
     field_label_classes = 'form-label-bold'
     error_summary_template_name = 'error-summary.html'
     auto_replace_widgets = True
     error_summary_title = 'There was a problem'
+    help_text = 'Please enter your DBS certificate number.'
+    correct_answer = {}
 
-    security_answer = None
+    dbs_number = DBSNumberField(label='DBS Number', required=True,
+                                error_messages={'required': 'Please give an answer'},
+                                widget=NannyFormInput)
 
-    def clean_security_answer(self):
-        security_answer = self.cleaned_data['security_answer']
+    def clean_dbs_number(self):
+        dbs_number = str(self.cleaned_data['dbs_number'])
 
-        if self.correct_answer.replace(' ', '') != security_answer.replace(' ', ''):
+        if self.correct_answer['dbs_number'].replace(' ', '') != dbs_number.replace(' ', ''):
             raise forms.ValidationError('Your answer must match what you told us in your application')
 
-        return security_answer
+        return dbs_number
 
 
-class DBSSecurityQuestionForm(BaseSecurityQuestionForm):
+class PersonalDetailsSecurityQuestionForm(GOVUKForm):
+    """
+    GOV.UK form for the personal details security question.
+    """
+    field_label_classes = 'form-label-bold'
+    error_summary_template_name = 'error-summary.html'
+    auto_replace_widgets = True
+    error_summary_title = 'There was a problem'
+    help_text = 'Please enter your postcode and date of birth'
+    correct_answer = {}
 
-    security_answer = DBSNumberField(label='DBS Number', required=True,
-                                     error_messages={'required': 'Please give an answer'},
-                                     widget=NannyFormInput)
-    help_text = 'Please enter your DBS certificate number.'
+    date_of_birth = CustomSplitDateFieldDOB(label='Date of birth',
+                                            help_text='For example, 31 03 1980',
+                                            error_messages={'required': 'Please give an answer'})
+
+    postcode = forms.CharField(label='Postcode',
+                               required=True,
+                               error_messages={'required': 'Please give an answer'},
+                               widget=NannyFormInput)
+
+    def clean_date_of_birth(self):
+        date_of_birth = str(self.cleaned_data['date_of_birth'])
+
+        if self.correct_answer['date_of_birth'].replace(' ', '') != date_of_birth.replace(' ', ''):
+            raise forms.ValidationError('Your answer must match what you told us in your application')
+
+        return date_of_birth
+
+    def clean_postcode(self):
+        postcode = str(self.cleaned_data['postcode'])
+
+        if self.correct_answer['postcode'].replace(' ', '') != postcode.replace(' ', ''):
+            raise forms.ValidationError('Your answer must match what you told us in your application')
+
+        return postcode
 
 
-class PersonalDetailsSecurityQuestionForm(BaseSecurityQuestionForm):
-
-    security_answer = None
-    help_text = None
-
-
-class MobileNumberSecurityQuestionForm(BaseSecurityQuestionForm):
-
-    security_answer = PhoneNumberField(label='Your mobile number',
-                                       number_type='mobile',
-                                       required=True,
-                                       error_messages={'required': 'Please give an answer'},
-                                       widget=NannyFormInput)
+class MobileNumberSecurityQuestionForm(GOVUKForm):
+    """
+    GOV.UK form for the mobile phone security question.
+    """
+    field_label_classes = 'form-label-bold'
+    error_summary_template_name = 'error-summary.html'
+    auto_replace_widgets = True
+    error_summary_title = 'There was a problem'
     help_text = 'Please enter your mobile number.'
+    correct_answer = {}
 
+    mobile_number = PhoneNumberField(label='Your mobile number',
+                                    number_type='mobile',
+                                    required=True,
+                                    error_messages={'required': 'Please give an answer'},
+                                    widget=NannyFormInput)
+
+    def clean_mobile_number(self):
+        mobile_number = str(self.cleaned_data['mobile_number'])
+
+        if self.correct_answer['mobile_number'].replace(' ', '') != mobile_number.replace(' ', ''):
+            raise forms.ValidationError('Your answer must match what you told us in your application')
+
+        return mobile_number
