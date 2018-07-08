738d05fda78891c399f0e575740fe6972b4d7d90
Thomas-95is <thomas.stokes@informed.com>
2018-07-08 12:47:35 +0100

    nanny_gateway.py: Create service-side class to expose API endpoints.

---
+++ nanny_gateway.py
@@ -0,0 +1,33 @@
+import os
+
+import coreapi
+
+
+class NannyGatewayActions:
+    client = coreapi.Client()
+    document = client.get(os.environ.get('APP_NANNY_GATEWAY_URL') + '/schema/')
+    target_url_prefix = os.environ.get('APP_NANNY_GATEWAY_URL') + '/api/v1/'
+
+    def list(self, endpoint, params):
+        action = [endpoint, 'list']
+        return self.client.action(self.document, action, params=params, overrides={'url': self.target_url_prefix + endpoint + '/' + params['application_id'] + '/'})
+
+    def read(self, endpoint, params):
+        action = [endpoint, 'read']
+        return self.client.action(self.document, action, params, overrides={'url': self.target_url_prefix + endpoint + '/' + params['application_id'] + '/'})
+
+    def create(self, endpoint, params):
+        action = [endpoint, 'create']
+        return self.client.action(self.document, action, params=params, overrides={'url': self.target_url_prefix + endpoint + '/' + params['application_id'] + '/'})
+
+    def patch(self, endpoint, params):
+        action = [endpoint, 'partial_update']
+        return self.client.action(self.document, action, params=params, overrides={'url': self.target_url_prefix + endpoint + '/' + params['application_id'] + '/'})
+
+    def put(self, endpoint, params):
+        action = [endpoint, 'update']
+        return self.client.action(self.document, action, params=params, overrides={'url': self.target_url_prefix + endpoint + '/' + params['application_id'] + '/'})
+
+    def delete(self, endpoint, params):
+        action = [endpoint, 'delete']
+        return self.client.action(self.document, action, params=params, overrides={'url': self.target_url_prefix + endpoint + '/' + params['application_id'] + '/'})
