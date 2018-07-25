866791871e178cf8f577f87e53cace21ffd7aae4
vagrant <thomas.stokes@informed.com>
2018-07-25 16:45:01 +0100

    date_of_birth.py: Factor out pip install models.

--- personal_details_app/views/date_of_birth.py
+++ personal_details_app/views/date_of_birth.py
@@ -1,11 +1,10 @@
 import datetime
-
-from nanny_models.nanny_application import NannyApplication
+from coreapi.exceptions import ErrorMessage
 
 from .BASE import BaseFormView
 from ..forms.date_of_birth import PersonalDetailsDOBForm
 
-from nanny_models.applicant_personal_details import ApplicantPersonalDetails
+from nanny_gateway import NannyGatewayActions
 
 from ..utils import app_id_finder
 
@@ -22,26 +21,14 @@ class PersonalDetailDOBView(BaseFormView):
         :return: a dictionary mapping form field names, to values of the correct type
         """
         initial = super().get_initial()
-
         application_id = app_id_finder(self.request)
-        try:
-            response = ApplicantPersonalDetails.api.get_record(application_id=application_id)
-            if response.status_code == 200:
-                personal_details_record = ApplicantPersonalDetails.api.get_record(application_id=application_id).record
-            elif response.status_code == 404:
-                return initial
-            print(response.status_code)
-        except TypeError:
-            return initial
-        try:
-            initial['date_of_birth'] = datetime.datetime.strptime(personal_details_record['date_of_birth'], '%Y-%m-%d')
-        except TypeError:
-            initial['date_of_birth'] = None
+        personal_details_record = NannyGatewayActions().read('applicant-personal-details', params={'application_id': application_id})
 
-        # If there has yet to be an entry for the model associated with the form, then no population necessary
+        if personal_details_record['date_of_birth'] is not None:
+            initial['date_of_birth'] = datetime.datetime.strptime(personal_details_record['date_of_birth'], '%Y-%m-%d')
 
         return initial
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
         app_id = app_id_finder(self.request)
@@ -49,22 +36,22 @@ class PersonalDetailDOBView(BaseFormView):
         return context
 
     def form_valid(self, form):
-
         application_id = app_id_finder(self.request)
-        application_record = NannyApplication.api.get_record(application_id=application_id).record
+        application_record = NannyGatewayActions().read('application', params={'application_id': application_id})
         application_record['personal_details_status'] = 'IN_PROGRESS'
-        NannyApplication.api.put(application_record)
+        NannyGatewayActions().patch('application', params=application_record)
 
         data_dict = {
             'application_id': application_id,
-            'date_of_birth': form.cleaned_data['date_of_birth'],
+            'date_of_birth': form.cleaned_data['date_of_birth'].strftime('%Y-%m-%d'),
         }
 
-        existing_record = ApplicantPersonalDetails.api.get_record(application_id=application_id)
-        if existing_record.status_code == 200:
-            del data_dict['application_id']
-            ApplicantPersonalDetails.api.put({**existing_record.record, **data_dict})
-        elif existing_record.status_code == 404:
-            ApplicantPersonalDetails.api.create(**data_dict, model_type=ApplicantPersonalDetails)
+        try:
+            NannyGatewayActions().patch('applicant-personal-details', params=data_dict)
+        except ErrorMessage as e:
+            if e.error.title == '404 Not Found':
+                NannyGatewayActions().create('applicant-personal-details', params=data_dict)
+            else:
+                raise e
 
         return super().form_valid(form)
