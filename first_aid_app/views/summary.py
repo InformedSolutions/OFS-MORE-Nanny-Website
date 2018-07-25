a0753b87b65f8a093f34db7e4519d174a6ef25d1
vagrant <thomas.stokes@informed.com>
2018-07-25 13:55:54 +0100

    summary.py: Factor out pip install models.

--- first_aid_app/views/summary.py
+++ first_aid_app/views/summary.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
-from nanny_models.nanny_application import NannyApplication
-from nanny_models.first_aid import FirstAidTraining
 
-from first_aid_app.views.base import BaseTemplateView, build_url
+from nanny_gateway import NannyGatewayActions
+
+from first_aid_app.views.base import build_url
 
 
 class Summary(View):
@@ -13,16 +13,14 @@ class Summary(View):
     success_url_name = 'Task-List'
 
     def get(self, request):
-
         context = self.get_context_data()
         return render(request, 'summary.html', context)
 
     def post(self, request):
-
         application_id = self.request.POST['id']
-        application_record = NannyApplication.api.get_record(application_id=application_id).record
+        application_record = NannyGatewayActions().read('application', params={'application_id': application_id})
         application_record['first_aid_training_status'] = 'COMPLETED'
-        NannyApplication.api.put(application_record)
+        NannyGatewayActions().put('application', application_record)
 
         return HttpResponseRedirect(build_url(self.success_url_name, get={'id': application_id}))
 
@@ -30,6 +28,6 @@ class Summary(View):
         context = {}
         application_id = self.request.GET['id']
         context['link_url'] = build_url(self.success_url_name, get={'id': application_id})
-        context['application_id'] = self.request.GET['id']
-        context['first_aid_record'] = FirstAidTraining.api.get_record(application_id=application_id).record
+        context['application_id'] = application_id
+        context['first_aid_record'] = NannyGatewayActions().read('first_aid_training', params={'application_id': application_id})
         return context
