fd88d0ca897bae3b0b67bd3c3c0e00903de88906
vagrant <thomas.stokes@informed.com>
2018-07-26 14:05:32 +0100

    where_you_work.py: Factor out pip install models.

--- childcare_address_app/views/where_you_work.py
+++ childcare_address_app/views/where_you_work.py
@@ -1,9 +1,11 @@
+from coreapi.exceptions import ErrorMessage
+
 from .base import BaseFormView
 from ..forms.where_you_work import WhereYouWorkForm
-from nanny_models.nanny_application import *
-from nanny_models.childcare_address import *
 from ..utils import *
 from django.shortcuts import HttpResponseRedirect
+
+from nanny_gateway import NannyGatewayActions
 
 
 class WhereYouWorkView(BaseFormView):
@@ -18,26 +20,28 @@ class WhereYouWorkView(BaseFormView):
     def form_valid(self, form):
         app_id = self.request.GET['id']
 
-        api_response = NannyApplication.api.get_record(application_id=app_id)
-        if api_response.status_code == 200:
-            record = api_response.record
+        record = NannyGatewayActions().read('application', params={'application_id': app_id})
 
-            address_response = ChildcareAddress.api.get_records(application_id=app_id)
+        if form.cleaned_data['address_to_be_provided'] == 'True':
+            record['address_to_be_provided'] = True
+            self.success_url = 'Childcare-Address-Location'
+        elif form.cleaned_data['address_to_be_provided'] == 'False':
+            record['address_to_be_provided'] = False
+            self.success_url = 'Childcare-Address-Details-Later'
+        else:
+            record['address_to_be_provided'] = None
 
-            if form.cleaned_data['address_to_be_provided'] == 'True':
-                record['address_to_be_provided'] = True
-                self.success_url = 'Childcare-Address-Location'
-            elif form.cleaned_data['address_to_be_provided'] == 'False':
-                record['address_to_be_provided'] = False
-                self.success_url = 'Childcare-Address-Details-Later'
+        record['childcare_address_status'] = 'IN_PROGRESS'
+        NannyGatewayActions().put('application', params=record)
+
+        try:
+            NannyGatewayActions().list('childcare-address', params={'application_id': app_id})
+            return HttpResponseRedirect(build_url('Childcare-Address-Details', get={'id': app_id}))
+        except ErrorMessage as e:
+            if e.error.title == '404 Not Found':
+                pass
             else:
-                record['address_to_be_provided'] = None
-
-            api_response.record['childcare_address_status'] = 'IN_PROGRESS'
-            NannyApplication.api.put(record)  # Update entire record.
-
-            if address_response.status_code != 404 and len(address_response.record) > 0:
-                return HttpResponseRedirect(build_url('Childcare-Address-Details', get={'id': app_id}))
+                raise e
 
         return super(WhereYouWorkView, self).form_valid(form)
 
