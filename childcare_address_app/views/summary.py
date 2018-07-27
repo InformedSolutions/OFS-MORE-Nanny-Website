60185df27b65cfffb88fc5bfe7367845d97ae4ce
vagrant <thomas.stokes@informed.com>
2018-07-27 09:40:21 +0100

    summary.py: Factor out pip install models.

--- childcare_address_app/views/summary.py
+++ childcare_address_app/views/summary.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.shortcuts import render
-from nanny_models.childcare_address import *
-from nanny_models.applicant_home_address import *
 from ..utils import build_url
 from ..address_helper import *
+
+from nanny_gateway import NannyGatewayActions
 
 
 class ChildcareAddressSummaryView(View):
@@ -17,54 +17,47 @@ class ChildcareAddressSummaryView(View):
             'id': app_id
         }
 
-        app_response = NannyApplication.api.get_record(application_id=app_id)
-        if app_response.status_code == 200:
+        app_record = NannyGatewayActions().read('application', params={'application_id': app_id})
 
-            app_record = app_response.record
-            if app_record['address_to_be_provided']:
-                address_to_be_provided = 'Yes'
-            else:
-                address_to_be_provided = 'No'
+        if app_record['address_to_be_provided']:
+            address_to_be_provided = 'Yes'
+        else:
+            address_to_be_provided = 'No'
 
-            context['address_to_be_provided'] = address_to_be_provided
+        context['address_to_be_provided'] = address_to_be_provided
 
-            address_response = ChildcareAddress.api.get_records(application_id=app_id)
+        address_records = NannyGatewayActions().list('childcare-address', params={'application_id': app_id})
 
-            if address_response.status_code == 200:
-                address_records = address_response.record
+        data = []
 
-                data = []
+        for i in range(1, len(address_records) + 1):
+            record = {}
+            record['title'] = "Childcare address " + str(i)
 
-                for i in range(1, len(address_records) + 1):
-                    record = {}
-                    record['title'] = "Childcare address " + str(i)
+            record['address'] = AddressHelper.format_address(address_records[i - 1], "</br>")
+            record['change_link'] = build_url('Childcare-Address-Manual-Entry',
+                                              get={
+                                                  'id': address_records[i - 1]['application_id'],
+                                                  'childcare_address_id': address_records[i - 1]['childcare_address_id']
+                                              })
+            data.append(record)
 
-                    record['address'] = AddressHelper.format_address(address_records[i - 1], "</br>")
-                    record['change_link'] = build_url('Childcare-Address-Manual-Entry',
-                                                      get={
-                                                          'id': address_records[i - 1]['application_id'],
-                                                          'childcare_address_id': address_records[i - 1]['childcare_address_id']
-                                                      })
-                    data.append(record)
-
-                context['records'] = data
+        context['records'] = data
 
         # get home address
-        home_address_resp = ApplicantHomeAddress.api.get_record(application_id=app_id)
-        if home_address_resp.status_code == 200:
-            home_address = home_address_resp.record['childcare_address']
-            if home_address:
-                context['home_address'] = 'Yes'
-            else:
-                context['home_address'] = 'No'
+        record = NannyGatewayActions().read('applicant-home-address', params={'application_id': app_id})
+
+        home_address = record['childcare_address']
+        if home_address:
+            context['home_address'] = 'Yes'
+        else:
+            context['home_address'] = 'No'
 
         return render(request, template_name='childcare-address-summary.html', context=context)
 
     def post(self, request):
         app_id = request.GET['id']
-        api_response = NannyApplication.api.get_record(
-            application_id=app_id
-        )
-        api_response.record['childcare_address_status'] = 'COMPLETED'
-        NannyApplication.api.put(api_response.record)
+        record = NannyGatewayActions().read('application', params={'application_id': app_id})
+        record['childcare_address_status'] = 'COMPLETED'
+        NannyGatewayActions().put('application', params=record)
         return HttpResponseRedirect(build_url('Task-List', get={'id': app_id}))
