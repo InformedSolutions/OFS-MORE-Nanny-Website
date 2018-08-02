5f4c6736cb422b24d9e16bd73073472cab33ae6f
vagrant <thomas.stokes@informed.com>
2018-08-02 17:24:14 +0100

    Fix broken first_aid_app_tests.

--- nanny/test_utils.py
+++ nanny/test_utils.py
@@ -7,6 +7,9 @@ mock_nanny_application = {
     'date_submitted': '2018-07-31 17:20:46.011717+00',
     'date_updated': '2018-07-31 17:20:46.011717+00',
     'childcare_training_status': 'NOT_STARTED',
+    'login_details_status': 'COMPLETED',
+    'personal_details_status': 'NOT_STARTED',
+    'criminal_record_check_status': 'NOT_STARTED',
 }
 
 mock_personal_details_record = {
@@ -23,6 +26,11 @@ mock_childcare_training_record = {
     'no_training': False
 }
 
+mock_dbs_record = {
+    'dbs_number': '000000000012',
+    'convictions': False,
+}
+
 mock_home_address = {
     'street_line1': 'Test',
     'street_line2': None,
@@ -31,8 +39,20 @@ mock_home_address = {
     'postcode': 'WA14 4PA'
 }
 
+mock_first_aid_record = {
+    'training_organisation': 'St Johns Ambulance',
+    'course_title': 'Pediatric First Aid',
+    'course_date': '2016-03-31'
+}
+
 mock_identity_record = {
-    'email': 'test@informed.com'
+    'email': 'test@informed.com',
+    'application_id': 'a4e6633f-5339-4de5-ae03-69c71fd008b3',
+    'magic_link_sms': '12345',
+    'sms_resend_attempts': 0,
+    'mobile_number': '000000000012',
+    'magic_link_email': 'ABCDEFGHIJKL',
+    'add_phone_number': '',
 }
 
 
@@ -52,6 +72,14 @@ home_address_response = HttpResponse()
 home_address_response.status_code = 200
 home_address_response.record = mock_home_address
 
+dbs_check_response = HttpResponse()
+dbs_check_response.status_code = 200
+dbs_check_response.record = mock_dbs_record
+
+first_aid_response = HttpResponse()
+first_aid_response.status_code = 200
+first_aid_response.record = mock_first_aid_record
+
 identity_response = HttpResponse()
 identity_response.status_code = 200
 identity_response.record = mock_identity_record
@@ -62,6 +90,8 @@ mock_endpoint_return_values = {
     'applicant-personal-details': personal_details_response,
     'childcare-training': childcare_training_response,
     'applicant-home-address': home_address_response,
+    'dbs-check': dbs_check_response,
+    'first-aid': first_aid_response,
     'user': identity_response,
 }
 
