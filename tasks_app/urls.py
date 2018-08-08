79bf957017fb3bf45238b1a3285c6f1c16c23020
vagrant <thomas.stokes@informed.com>
2018-08-08 11:26:01 +0100

    urls.py: Configure application cancelled views to templates.

--- tasks_app/urls.py
+++ tasks_app/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.conf.urls import url, include
 
 from .views import *
@@ -6,6 +5,8 @@ from .views import *
 
 urlpatterns = [
     url(r'^task-list/', TaskListView.as_view(), name='Task-List'),
+    url(r'cancel-application/', CancelApplicationTemplateView.as_view(), name='Cancel-Application'),
+    url(r'application-cancelled/', ApplicationCancelledTemplateView.as_view(), name='Application-Cancelled'),
     url(r'^insurance-cover/', InsuranceCoverGuidanceView.as_view(), name='Insurance-Cover-Guidance'),
     url(r'^first-aid-training/', FirstAidGuidanceView.as_view(), name='First-Aid-Guidance'),
     url(r'^costs/', CostsView.as_view(), name='Costs'),
