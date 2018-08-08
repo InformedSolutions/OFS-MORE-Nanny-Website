9ddaf7842295813f67760b2d51dc6c288e3deb31
vagrant <thomas.stokes@informed.com>
2018-08-08 11:21:47 +0100

    test_cancel_application.py: Add test for session destruction.

--- tasks_app/tests/test_views/test_cancel_application.py
+++ tasks_app/tests/test_views/test_cancel_application.py
@@ -1,21 +1,25 @@
 from http.cookies import SimpleCookie
 from unittest import mock
+import uuid
 
-from django.shortcuts import reverse
 from django.test import modify_settings, TestCase
+from django.urls import resolve, reverse
 
-from tasks_app.views import ApplcationCancelledTemplateView, CancelApplicationTemplateView
+from nanny.middleware import CustomAuthenticationHandler
+
+from tasks_app.views import ApplicationCancelledTemplateView, CancelApplicationTemplateView
 
 
 @modify_settings(MIDDLEWARE={
         'remove': [
-            'middleware.CustomAuthenticationHandler',
+            'nanny.middleware.CustomAuthenticationHandler',
         ]
     })
 class TestCancelApplication(TestCase):
 
     def setUp(self):
         self.client.cookies = SimpleCookie({'_ofs': 'test@informed.com'})
+        self.example_uuid = str(uuid.uuid4())
 
     # ---------- #
     # HTTP Tests #
@@ -25,7 +29,7 @@ class TestCancelApplication(TestCase):
         """
         Test applicant can render the 'Cancel-Application' page.
         """
-        response = self.client.get(reverse('Cancel-Application'))
+        response = self.client.get(reverse('Cancel-Application') + '?id=' + self.example_uuid)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.func.__name__, CancelApplicationTemplateView.as_view().__name__)
@@ -37,13 +41,25 @@ class TestCancelApplication(TestCase):
         with mock.patch('nanny.db_gateways.NannyGatewayActions.delete') as nanny_api_delete, \
             mock.patch('nanny.db_gateways.IdentityGatewayActions.delete') as identity_api_delete:
 
-            nanny_api_delete.return_value.status_code = 200
-            identity_api_delete.return_value.status_code = 200
+            nanny_api_delete.return_value.status_code = 204
+            identity_api_delete.return_value.status_code = 204
 
-            response = self.client.post(reverse('Cancel-Application'))
+            response = self.client.post(reverse('Cancel-Application') + '?id=' + self.example_uuid)
 
             self.assertTrue(nanny_api_delete.called)
             self.assertTrue(identity_api_delete.called)
 
             self.assertEqual(response.status_code, 302)
-            self.assertEqual(response.resolver_match.func.__name__, ApplcationCancelledTemplateView.as_view().__name__))
+            self.assertEqual(resolve(response.url).func.view_class, ApplicationCancelledTemplateView)
+
+    def test_session_destroyed_upon_application_cancellation(self):
+        """
+        Test applicant has their session cookies destroyed upon cancelling their application.
+        """
+        with mock.patch('nanny.db_gateways.NannyGatewayActions.delete'), \
+            mock.patch('nanny.db_gateways.IdentityGatewayActions.delete'):
+
+            response = self.client.post(reverse('Cancel-Application') + '?id=' + self.example_uuid)
+            cookie_key = CustomAuthenticationHandler.get_cookie_identifier()
+
+            self.assertEqual(response.cookies[cookie_key].value, '')
