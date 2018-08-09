9eaf20d12cc81100e6af01159c1021dbe9a55eee
Alice King <alice.king@informed.com>
2018-08-09 12:22:01 +0100

    set environ var to None rather than removing its assignment

--- nanny/notify.py
+++ nanny/notify.py
@@ -20,6 +20,13 @@ def send_email(email, personalisation, template_id):
     # If executing function in test mode override email address
     if settings.EXECUTING_AS_TEST == 'True':
         email = 'simulate-delivered@notifications.service.gov.uk'
+        # If executing login function in test mode set env variable for later retrieval by test code
+        os.environ['EMAIL_VALIDATION_URL'] = personalisation.get('link')
+    else:
+        try:
+            print(personalisation['link'])
+        except:
+            pass
 
     notification_request = {
         'service_name': 'Nannies',
