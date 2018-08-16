4cef5c06617b4ddeacb35dffb10f77099ed98f20
LRydeard <luke.rydeard@informed.com>
2018-08-16 16:42:52 +0100

    Added change email functionality and journey

--- login_app/views/change_email.py
+++ login_app/views/change_email.py
@@ -7,11 +7,12 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 
 from nanny import notify
+from nanny.utilities import generate_email_validation_link
 from login_app.forms import ContactEmailForm
 from nanny import utilities
 from .base import BaseFormView
 
-from nanny.db_gateways import IdentityGatewayActions
+from nanny.db_gateways import IdentityGatewayActions, NannyGatewayActions
 
 
 class ChangeEmailTemplateView(BaseFormView):
@@ -20,6 +21,7 @@ class ChangeEmailTemplateView(BaseFormView):
     """
     template_name = 'change-email.html'
     form_class = ContactEmailForm
+    check_answers_url = 'Contact-Details-Summary'
     success_url = 'Check-New-Email'
     email_address = None
 
@@ -30,11 +32,20 @@ class ChangeEmailTemplateView(BaseFormView):
         application_id = self.request.GET.get('id')
         self.email_address = form.cleaned_data['email_address']
 
+        # Create GatewayActions instances
         identity_actions = IdentityGatewayActions()
+        nanny_actions = NannyGatewayActions()
+
+        # Get relevant records
         user_identity_record = identity_actions.read('user',
                                                      params={'application_id': application_id}).record
+        personal_details_record = nanny_actions.read('applicant-personal-details',
+                                                     params={'application_id': application_id}).record
 
+        # Get user's current email and first name
         account_email = user_identity_record['email']
+        first_name = personal_details_record['first_name']
+
 
         existing_account_response = identity_actions.list('user', params={'email': self.email_address})
         existing_account_response_status_code = existing_account_response.status_code
@@ -43,21 +54,53 @@ class ChangeEmailTemplateView(BaseFormView):
 
         if self.email_address == account_email:
             # If email is unchanged, return to the sign-in details check-answers page
-            return HttpResponseRedirect(reverse('Placeholder') + '?id=' + application_id)
+            same_email_redirect = utilities.build_url(self.check_answers_url,
+                                                          get={'id': application_id})
+            return HttpResponseRedirect(same_email_redirect)
 
         elif email_in_use:
             # If the email is already being used,
             if settings.DEBUG:
-                print(
-                    "You will not see an email validation link printed because an account already exists with that email.")
-            return HttpResponseRedirect(reverse('Placeholder') + '?email=' + self.email_address)
+                print("You will not see an email validation link printed because an account already exists with that email.")
+            not_sent_email_redirect = utilities.build_url(self.success_url,
+                                                      get={'email_address': self.email_address, 'id': application_id})
+            return HttpResponseRedirect(not_sent_email_redirect)
 
         else:
-            # TODO: Send update email, redirect to page.
-            pass
-            # # Send an email to the new email adddress, with the account's ID in the link.
-            # update_magic_link(email, app_id)
-            # redirect_url = build_url('Update-Email-Sent', get={'email': email, 'id': app_id})
-            # return HttpResponseRedirect(redirect_url)
+            # Generate a new magic link and expiry date
+            validation_link, email_expiry_date = generate_email_validation_link(self.email_address)
+            magic_link = validation_link.split('/')[-1]
+            validation_link += '?email=' + self.email_address
 
-        # return HttpResponseRedirect(self.get_success_url())
+            # Create an update record with the magic_link information
+            email_update_record = user_identity_record
+            email_update_record['magic_link_email'] = magic_link
+            email_update_record['email_expiry_date'] = email_expiry_date
+
+            # Update the user record
+            IdentityGatewayActions().put('user', params=email_update_record)
+
+            # Send the 'Change Email' email
+            if settings.DEBUG:
+                print(validation_link)
+            else:
+                self.send_change_email_email(self.email_address, first_name, validation_link)
+
+            sent_email_redirect = utilities.build_url(self.success_url,
+                                               get={'email_address': self.email_address, 'id': application_id})
+            return HttpResponseRedirect(sent_email_redirect)
+
+    def send_change_email_email(self, email, first_name, url):
+        """
+        Method to send a 'Change Email' email using the Notify Gateway API
+        :param email: string containing the e-mail address to send the e-mail to
+        :param first_name: string first name
+        :param magic_link: url directing to email validation page.
+        :return: HTTP response
+        """
+        if hasattr(settings, 'NOTIFY_URL'):
+            email = str(email)
+            template_id = '108ecfa0-4496-4ce7-97c8-f43c9f42a374'
+            personalisation = {'first_name': first_name, 'magic_link': url}
+            return notify.send_email(email, personalisation, template_id)
+
