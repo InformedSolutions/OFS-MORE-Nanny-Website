ba3e029551f3ff4ad53f2eb3eab61255ce7bd990
vagrant <thomas.stokes@informed.com>
2018-08-20 11:44:42 +0100

    summary.py: Update to use generic-summary-template.html

--- first_aid_app/views/summary.py
+++ first_aid_app/views/summary.py
@@ -5,6 +5,7 @@ from django.views import View
 from first_aid_app.views.base import build_url
 
 from nanny.db_gateways import NannyGatewayActions
+from nanny.table_util import Row, Table
 
 
 class Summary(View):
@@ -13,8 +14,7 @@ class Summary(View):
     success_url_name = 'Task-List'
 
     def get(self, request):
-        context = self.get_context_data()
-        return render(request, 'summary.html', context)
+        return render(request, 'generic-summary-template.html', context=self.get_context_data())
 
     def post(self, request):
         application_id = self.request.POST['id']
@@ -23,10 +23,21 @@ class Summary(View):
         NannyGatewayActions().put('application', params=application_record)
         return HttpResponseRedirect(build_url(self.success_url_name, get={'id': application_id}))
 
-    def get_context_data(self, **kwargs):
-        context = {}
+    def get_context_data(self):
+        context = dict()
         application_id = self.request.GET['id']
-        context['link_url'] = build_url(self.success_url_name, get={'id': application_id})
-        context['application_id'] = self.request.GET['id']
-        context['first_aid_record'] = NannyGatewayActions().read('first-aid', params={'application_id': application_id}).record
+        first_aid_record = NannyGatewayActions().read('first-aid', params={'application_id': application_id}).record
+
+        organistaion_row = Row('training_organisation', 'Training organisation', first_aid_record['training_organisation'], 'first-aid:Training-Details')
+        course_title_row = Row('course_title', 'Title of training course', first_aid_record['course_title'], 'first-aid:Training-Details')
+        course_date_row  = Row('course_date', 'Date you completed the course', first_aid_record['course_date'], 'first-aid:Training-Details')
+
+        first_aid_table = Table(application_id)
+        first_aid_table.row_list = [organistaion_row, course_title_row, course_date_row]
+        first_aid_table.get_errors()
+
+        context['table_list'] = [first_aid_table]
+        context['application_id'] = application_id
+        context['page_title'] = 'Check your answers: first aid training'
+
         return context
