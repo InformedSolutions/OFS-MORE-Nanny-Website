b0b114d3a3e197032b23fc8ec24ad9340f547598
proctorj <james.proctor@informed.com>
2018-10-09 11:22:29 +0100

    Created new view for childcare training certificate, updated 'urls.py' and 'init.py', changed success/redirect urls

--- childcare_training_task_app/urls.py
+++ childcare_training_task_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^childcare-training/type/', views.TypeOfChildcareTrainingFormView.as_view(), name='Type-Of-Childcare-Training'),
     url(r'^childcare-training/check-answers/', views.ChildcareTrainingSummaryView.as_view(), name='Childcare-Training-Summary'),
     url(r'^childcare-training-course/', views.ChildcareTrainingCourseView.as_view(), name='Childcare-Training-Course'),
+    url(r'^childcare-training-certificate/', views.ChildcareTrainingCertificateView.as_view(), name='Childcare-Training-Certificate'),
 ]
 
 # Django toolbar settings for development environments
---
+++ childcare_training_task_app/views/childcare_training_certificate.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, reverse
+from django.views import View
+
+from nanny.db_gateways import NannyGatewayActions
+
+
+class ChildcareTrainingCertificateView(View):
+    """
+    Class containing the methods for handling requests to the 'Childcare-Training-Certificate' page.
+    """
+    def get(self, request):
+        context = {'id': request.GET['id']}
+        return render(request, template_name='childcare-training-certificate.html', context=context)
+
+    def post(self, request):
+        application_id = request.GET['id']
+        record = NannyGatewayActions().read('application', params={'application_id':application_id}).record
+        record['childcare_training_status'] = 'IN_PROGRESS'
+        NannyGatewayActions().put('application', params=record)
+        return HttpResponseRedirect(reverse('Childcare-Training-Summary') + '?id=' + application_id)
