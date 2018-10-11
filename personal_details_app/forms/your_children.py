13e8ea37313a109528b199581c9a1e36746387ab
proctorj <james.proctor@informed.com>
2018-10-11 15:06:26 +0100

    Created 'your_children.py'as a form and added form content

---
+++ personal_details_app/forms/your_children.py
@@ -0,0 +1,28 @@
+from django import forms
+
+from govuk_forms.widgets import InlineRadioSelect
+
+from nanny.utilities import NannyForm
+
+
+class PersonalDetailsYourChildrenForm(NannyForm):
+    """
+    GOV.UK form for specifying whether or not the applicant has children of their own who are under 16 years old.
+    """
+    field_label_classes = 'form-label-bold'
+    error_summary_template_name = 'standard-error-summary.html'
+    error_summary_title = 'There was a problem'
+    auto_replace_widgets = True
+
+    options = (
+        (True, 'Yes'),
+        (False, 'No')
+    )
+
+    child_under16 = forms.ChoiceField(
+        label='Do you have children of your own under 16?',
+        choices=options,
+        widget=InlineRadioSelect,
+        required=True,
+        error_messages={'required': 'Please say if you have children of your own under 16'}
+    )
