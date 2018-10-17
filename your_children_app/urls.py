168d03f3583da52a51146d42614441fe66a7a274
proctorj <james.proctor@informed.com>
2018-10-17 16:50:51 +0100

    Added shell task with pages/view for redirect development basis

--- your_children_app/urls.py
+++ your_children_app/urls.py
@@ -2,11 +2,22 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 
-from .views import YourChildrenGuidance
+from tasks_app.views import TaskListView
+from tasks_app.views.your_children import YourChildrenGuidance
+from your_children_app.views.your_children_address import YourChildrenAddressView, YourChildrenManualAddressView
+from your_children_app.views.your_children_addresses import YourChildrenAddressesView
+from your_children_app.views.your_children_details import YourChildrenDetailsView
+from your_children_app.views.your_children_summary import YourChildrenManualSummaryView
 
 urlpatterns = [
     url(r'^your_children/your_children/', YourChildrenGuidance.as_view(), name='Your-Children-Guidance'),
-
+    url(r'^your_children_details/', YourChildrenDetailsView.as_view(), name='Your-Children-Details'),
+    url(r'^your_children/addresses/', YourChildrenAddressesView.as_view(), name='Your-Children-addresses'),
+    url(r'^your_children/address/', YourChildrenAddressView.as_view(), name='Your-Children-address'),
+    url(r'^your_children/enter_address/', YourChildrenManualAddressView.as_view(), name='Your-Children-Manual-address'),
+    url(r'^your_children/check_answers/', YourChildrenManualSummaryView.as_view(), name='Your-Children-Summary'),
+    url(r'^your_children/check_answers/', YourChildrenManualSummaryView.as_view(), name='Your-Children-Summary'),
+    url(r'^task-list/', TaskListView.as_view(), name='Task-List'),
 ]
 
 # Django toolbar settings for development environments
