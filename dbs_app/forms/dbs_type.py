6303c2a805053f85bf451b936e6d2c76c15bd1c0
James Cruddas <james.cruddas@informed.com>
2018-10-19 12:25:32 +0100

    dbs_type.py: Add form for declaring DBS type.

---
+++ dbs_app/forms/dbs_type.py
@@ -0,0 +1,28 @@
+from django import forms
+
+from govuk_forms.widgets import InlineRadioSelect
+
+from nanny.utilities import NannyForm
+
+
+class DBSTypeForm(NannyForm):
+    """
+    GOV.UK form for the 'Lived Abroad' Page
+    """
+    field_label_classes = 'form-label-bold'
+    error_summary_template_name = 'standard-error-summary.html'
+    error_summary_title = 'There was a problem'
+    auto_replace_widgets = True
+
+    options = (
+        ('True', 'Yes'),
+        ('False', 'No')
+    )
+
+    is_ofsted_dbs = forms.ChoiceField(
+            label='Do you have an Ofsted DBS Check?',
+            choices=options,
+            widget=InlineRadioSelect,
+            required=True,
+            error_messages={'required': 'Please say if you have an Ofsted DBS check'}
+    )
