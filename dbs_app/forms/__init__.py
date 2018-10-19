6ef35de7bb4e45c56e69345f151095c0aeaca561
James Cruddas <james.cruddas@informed.com>
2018-10-19 12:26:00 +0100

    __init__.py: Add new forms to init import.

--- dbs_app/forms/__init__.py
+++ dbs_app/forms/__init__.py
@@ -1,1 +1,3 @@
-from .dbs_details import DBSDetailsForm
+from .dbs_details import NonCapitaDBSDetailsForm, CaptiaDBSDetailsForm
+from .lived_abroad import LivedAbroadForm
+from .dbs_type import DBSTypeForm
