14660bd0bc5c519e5294a9eaacf88ac01caeafd9
Thomas-95IS <thomas.stokes@informed.com>
2018-10-22 15:09:12 +0100

    dbs_upload.py: Remove defunct view.

--- dbs_app/views/dbs_upload.py
+++
@@ -1,9 +0,0 @@
-from nanny.base_views import NannyTemplateView
-
-
-class DBSUpload(NannyTemplateView):
-    """
-    Template view to  render the post dbs page from the dbs task
-    """
-    template_name = "dbs-upload.html"
-    success_url_name = 'dbs:Summary'
