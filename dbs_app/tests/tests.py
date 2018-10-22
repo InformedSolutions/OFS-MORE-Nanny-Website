61e350fc2e00088ed270810446e32d82aea206bf
James Cruddas <james.cruddas@informed.com>
2018-10-22 11:23:38 +0100

    tests.py: Add assertions for all tests.

--- dbs_app/tests/tests.py
+++ dbs_app/tests/tests.py
@@ -1,11 +1,14 @@
+from unittest import mock
 from uuid import uuid4
 
 from django.forms import ValidationError
 from django.shortcuts import reverse
-from django.test import Client, modify_settings, TestCase
+from django.test import Client, modify_settings, SimpleTestCase
 from django.urls import resolve
 
-from dbs_app import forms as dbs_forms#, views
+from dbs_app import forms as dbs_forms, views
+from nanny.db_gateways import NannyGatewayActions
+from nanny.test_utils import side_effect, mock_nanny_application
 from tasks_app.views import TaskListView
 
 
@@ -14,7 +17,13 @@ from tasks_app.views import TaskListView
             'middleware.CustomAuthenticationHandler',
         ]
     })
-class CriminalRecordChecksTest(TestCase):
+@mock.patch.object(NannyGatewayActions, 'create', side_effect=side_effect)
+@mock.patch.object(NannyGatewayActions, 'read',   side_effect=side_effect)
+@mock.patch.object(NannyGatewayActions, 'list',   side_effect=side_effect)
+@mock.patch.object(NannyGatewayActions, 'patch',  side_effect=side_effect)
+@mock.patch.object(NannyGatewayActions, 'put',    side_effect=side_effect, name='put')
+@mock.patch.object(NannyGatewayActions, 'delete', side_effect=side_effect)
+class CriminalRecordChecksTest(SimpleTestCase):
     @classmethod
     def setUpClass(cls):
         super(CriminalRecordChecksTest, cls).setUpClass()
@@ -27,130 +36,164 @@ class CriminalRecordChecksTest(TestCase):
     # Test View routing and rendering #
     # ------------------------------- #
 
-    def test_can_render_guidance_page(self):
+    def test_can_render_guidance_page(self, *args):
         response = self.client.get(reverse('dbs:Criminal-Record-Checks-Guidance-View'))
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.__name__, views.CriminalRecordsCheckGuidanceView)
         self.assertTemplateUsed('criminal-record-checks-guidance.html')
 
-    def test_post_request_to_guidance_page_redirects_to_lived_abroad_page(self):
+    def test_post_request_to_guidance_page_redirects_to_lived_abroad_page(self, *args):
         response = self.client.post('dbs:Guidance-View')
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(resolve(response.url).func.__name__, views.LivedAbroadFormView)
 
-    def test_post_request_to_guidance_page_sets_task_status_to_started(self):
-        self.skipTest('NotImplemented')
+    def test_post_request_to_guidance_page_sets_task_status_to_in_progress(self, *args):
+        self.client.post('dbs:Guidance-View')
+        put_mock = args[1]
 
-    def test_can_render_lived_abroad_page(self):
+        expected_call_args = mock_nanny_application
+        expected_call_args['dbs_status'] = 'IN_PROGRESS'
+
+        self.assertTrue(put_mock.called)
+        self.assertEqual(1, len(put_mock.call_args_list))
+        self.assertEqual(put_mock.call_args, expected_call_args)
+
+    def test_can_render_lived_abroad_page(self, *args):
         response = self.client.get(reverse('dbs:Lived-Abroad-View'))
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.__name__, views.LivedAbroadFormView)
         self.assertTemplateUsed('lived-abroad.html')
 
-    def test_no_to_lived_abroad_redirects_to_dbs_guidance_page(self):
+    def test_no_to_lived_abroad_redirects_to_dbs_guidance_page(self, *args):
         response = self.client.post(reverse('dbs:Lived-Abroad-View'), data={'lived_abroad': False})
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(resolve(response.url).__name__, views.DBSGuidanceView)
 
-    def test_yes_to_lived_abroad_redirects_to_certificates_of_good_conduct_page(self):
+    def test_yes_to_lived_abroad_redirects_to_certificates_of_good_conduct_page(self, *args):
         response = self.client.post(reverse('dbs:Lived-Abroad-View'), data={'lived_abroad': True})
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(resolve(response.url).__name__, views.GoodConductView)
 
-    def test_post_request_to_certificates_of_good_conduct_page_redirects_to_post_good_conduct_certificates_page(self):
+    def test_post_request_to_certificates_of_good_conduct_page_redirects_to_post_good_conduct_certificates_page(self, *args):
         response = self.client.post(reverse('dbs:Good-Conduct-View'))
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(resolve(response.url).__name__, views.GoodConductView)
 
-    def test_can_render_post_good_conduct_certificates_page(self):
+    def test_can_render_post_good_conduct_certificates_page(self, *args):
         response = self.client.get(reverse('dbs:Good-Conduct-View'))
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.__name__, views.PostGoodConductCertificatesView)
         self.assertTemplateUsed('post-good-conduct-certificates.html')
 
-    def test_post_request_to_post_good_conduct_certificates_page_redirects_to_dbs_guidance(self):
+    def test_post_request_to_post_good_conduct_certificates_page_redirects_to_dbs_guidance(self, *args):
         # TODO: Check this doesn't involve an actual form.
         self.skipTest('NotImplemented')
 
-    def test_can_render_dbs_guidance_page(self):
+    def test_can_render_dbs_guidance_page(self, *args):
         response = self.client.get(reverse('dbs:DBS-Guidance-View'))
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.__name__, views.DBSGuidanceView)
         self.assertTemplateUsed('dbs-guidance.html')
 
-    def test_post_request_to_dbs_guidance_page_redirects_to_dbs_type_page(self):
+    def test_post_request_to_dbs_guidance_page_redirects_to_dbs_type_page(self, *args):
         response = self.client.post(reverse('dbs:DBS-Guidance-View'))
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(resolve(response.url).__name__, views.DBSGuidanceView)
 
-    def test_can_render_dbs_type_page(self):
+    def test_can_render_dbs_type_page(self, *args):
         response = self.client.get(reverse('dbs:DBS-Type-View'))
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.__name__, views.DBSTypeFormView)
         self.assertTemplateUsed('dbs-type.html')
 
-    def test_capita_dbs_to_dbs_type_page_redirects_to_capita_dbs_details_page(self):
+    def test_capita_dbs_to_dbs_type_page_redirects_to_capita_dbs_details_page(self, *args):
         response = self.client.post(reverse('dbs:DBS-Type-View'), data={'is_ofsted_dbs': True})
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(resolve(response.url).__name__, views.CaptitaDBSDetailsFormView)
 
-    def test_non_capita_dbs_to_dbs_type_page_redirects_to_dbs_update_service_page(self):
+    def test_non_capita_dbs_to_dbs_type_page_redirects_to_dbs_update_service_page(self, *args):
         response = self.client.post(reverse('dbs:DBS-Type-View'), data={'is_ofsted_dbs': False})
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(resolve(response.url).__name__, views.DBSUpdateServiceFormView)
 
-    def test_can_render_capita_dbs_details_page(self):
+    def test_can_render_capita_dbs_details_page(self, *args):
         response = self.client.get(reverse('dbs:Capita-DBS-Details-View'))
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.__name__, views.CaptitaDBSDetailsFormView)
         self.assertTemplateUsed('capita-dbs-details.html')
 
-    def test_cautions_and_convictions_on_capita_dbs_details_page_redirects_to_post_dbs_certificate_page(self):
-        self.skipTest('NotImplemented')
+    def test_cautions_and_convictions_on_capita_dbs_details_page_redirects_to_post_dbs_certificate_page(self, *args):
+        response = self.client.post(reverse('dbs:Capita-DBS-Details-View'),
+                                    data={
+                                        'dbs_number': '000000000012',
+                                        'has_convictions': True,
+                                    })
 
-    def test_no_cautions_and_convicitons_on_capita_dbs_details_page_redirects_to_summmary_page(self):
-        self.skipTest('NotImplemented')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(resolve(response.url).__name__, views.PostDBSCertificateView)
 
-    def test_can_render_post_dbs_certificate_page(self):
+    def test_no_cautions_and_convictions_on_capita_dbs_details_page_redirects_to_summmary_page(self, *args):
+        response = self.client.post(reverse('dbs:Capita-DBS-Details-View'),
+                                    data={
+                                        'dbs_number': '000000000012',
+                                        'has_convictions': False,
+                                    })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(resolve(response.url).__name__, views.CriminalRecordChecksSummaryView)
+
+    def test_can_render_post_dbs_certificate_page(self, *args):
         response = self.client.get(reverse('dbs:Post-DBS-Certificate'))
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.__name__, views.PostDBSCertificateView)
         self.assertTemplateUsed('post-dbs-certificate.html')
 
-    def test_post_request_to_post_dbs_certificate_page_redirects_to_summary_page(self):
+    def test_post_request_to_post_dbs_certificate_page_redirects_to_summary_page(self, *args):
         response = self.client.post(reverse('dbs:Post-DBS-Certificate'))
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(resolve(response.url).__name__, CriminalRecordChecksSummaryView)
+        self.assertEqual(resolve(response.url).__name__, views.CriminalRecordChecksSummaryView)
 
-    def test_can_render_dbs_update_service_page(self):
+    def test_can_render_dbs_update_service_page(self, *args):
         response = self.client.get(reverse('dbs:DBS-Update-Service-Page'))
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.__name__, views.DBSUpdateServiceFormView)
         self.assertTemplateUsed('dbs-update-service.html')
 
-    def test_yes_to_dbs_update_serice_page_redirects_to_non_capita_dbs_details_page(self):
-        self.skipTest('NotImplemented')
+    def test_yes_to_dbs_update_serice_page_redirects_to_non_capita_dbs_details_page(self, *args):
+        response = self.client.post(reverse('dbs:DBS-Update-Service-Page'),
+                                    data={
+                                        'on_update_service': True
+                                    })
 
-    def test_no_to_dbs_update_page_redirects_to_get_a_dbs_page(self):
-        self.skipTest('NotImplemented')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(resolve(response.url).__name__, views.NonCatitaDBSDetailsFormView)
 
-    def test_can_render_get_a_dbs_page(self):
+    def test_no_to_dbs_update_page_redirects_to_get_a_dbs_page(self, *args):
+        response = self.client.post(reverse('dbs:DBS-Update-Service-Page'),
+                                    data={
+                                        'on_update_service': False
+                                    })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(resolve(response.url).__name__, views.GetDBSView)
+
+    def test_can_render_get_a_dbs_page(self, *args):
         response = self.client.get(reverse('dbs:Get-A-DBS-View'))
 
         self.assertEqual(response.status_code, 200)
@@ -163,39 +206,59 @@ class CriminalRecordChecksTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(resolve(response.url).__name__, TaskListView)
 
-    def test_post_request_to_get_a_dbs_page_sets_task_status_to_started(self):
-        self.skipTest('NotImplemented')
+    def test_post_request_to_get_a_dbs_page_sets_task_status_to_in_progress(self, *args):
+        self.client.post(reverse('dbs:Get-A-DBS-View'))
+        put_mock = args[1]
 
-    def test_can_render_non_captita_dbs_details_page(self):
+        expected_call_args = mock_nanny_application
+        expected_call_args['dbs_status'] = 'IN_PROGRESS'
+
+        self.assertTrue(put_mock.called)
+        self.assertEqual(1, len(put_mock.call_args_list))
+        self.assertEqual(put_mock.call_args, expected_call_args)
+
+    def test_can_render_non_captita_dbs_details_page(self, *args):
         response = self.client.get(reverse('dbs:Non-Captia-DBS-Details-View'))
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.__name__, views.NonCatitaDBSDetailsFormView)
         self.assertTemplateUsed('non-capita-dbs-details.html')
 
-    def test_post_to_non_captita_dbs_details_page_redirects_to_post_dbs_certificate_page(self):
-        self.skipTest('NotImplemented')
+    def test_post_to_non_captita_dbs_details_page_redirects_to_post_dbs_certificate_page(self, *args):
+        response = self.client.post(reverse('dbs:Non-Captia-DBS-Details-View'),
+                                    data={
+                                        'dbs_number': '000000000012'
+                                    })
 
-    def test_can_render_summary_page(self):
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(resolve(response.url).__name__, views.PostDBSCertificateView)
+
+    def test_can_render_summary_page(self, *args):
         response = self.client.get(reverse('dbs:Criminal-Record-Check-Summary-View'))
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.__name__, views.CriminalRecordChecksSummaryView)
         self.assertTemplateUsed('criminal-record-checks-summary.html')
 
-    def test_post_request_to_summary_page_redirects_to_task_list(self):
+    def test_post_request_to_summary_page_redirects_to_task_list(self, *args):
         response = self.client.post(reverse('dbs:Criminal-Record-Check-Summary-View'))
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.resolver_match.__name__, TaskListView)
 
-    def test_post_request_to_summary_page_sets_task_status_to_completed(self):
-        self.skipTest('NotImplemented')
+    def test_post_request_to_summary_page_sets_task_status_to_completed(self, *args):
+        self.client.post(reverse('dbs:Criminal-Record-Check-Summary-View'))
+        put_mock = args[1]
 
-    # ---------- #
-    # Test forms #
-    # ---------- #
+        expected_call_args = mock_nanny_application
+        expected_call_args['dbs_status'] = 'COMPLETED'
 
+        self.assertTrue(put_mock.called)
+        self.assertEqual(1, len(put_mock.call_args_list))
+        self.assertEqual(put_mock.call_args, expected_call_args)
+
+
+class CriminalRecordFormsTest(SimpleTestCase):
     def test_less_than_12_digit_dbs_raises_error(self):
         form = dbs_forms.NonCapitaDBSDetailsForm(data={'dbs_number': '1'})
 
@@ -212,7 +275,7 @@ class CriminalRecordChecksTest(TestCase):
         form = dbs_forms.NonCapitaDBSDetailsForm(data={'dbs_number': ''})
 
         with self.assertRaisesMessage(ValidationError, 'Please enter your DBS certificate number'):
-            form.fields['dbs_number'].clean()
+            form.fields['dbs_number'].clean('')
 
     def test_entering_a_12_digit_dbs_number_does_not_raise_error(self):
         form = dbs_forms.NonCapitaDBSDetailsForm(data={'dbs_number': '012345678912'})
@@ -226,7 +289,10 @@ class CriminalRecordChecksTest(TestCase):
             form.fields['lived_abroad'].clean('')
 
     def test_not_entering_an_option_for_on_update_raises_error(self):
-        self.skipTest('NotImplemented')
+        form = dbs_forms.DBSUpdateService(data={'on_update_service': ''})
+
+        with self.assertRaisesMessage(ValidationError, 'Please say if you are on the DBS update service'):
+            form.fields['on_update_service'].clean('')
 
     def test_not_entering_an_option_for_dbs_type_raises_error(self):
         form = dbs_forms.DBSTypeForm(data={'is_ofsted_dbs': ''})
