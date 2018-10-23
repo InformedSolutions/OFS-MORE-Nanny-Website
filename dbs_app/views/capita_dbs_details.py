2bcee5d228eb74b1069bb38aea2e5ad40830047b
Thomas-95IS <thomas.stokes@informed.com>
2018-10-23 13:47:36 +0100

    capita_dbs_details.py: Fix for str form data.

--- dbs_app/views/capita_dbs_details.py
+++ dbs_app/views/capita_dbs_details.py
@@ -16,12 +16,12 @@ class CapitaDBSDetailsFormView(NannyFormView):
         dbs_number = form.cleaned_data['dbs_number']
         has_convictions = form.cleaned_data['has_convictions']
 
-        if has_convictions:
+        if has_convictions == 'True':
             self.success_url = 'dbs:Post-DBS-Certificate'
-        elif not has_convictions:
+        elif has_convictions == 'False':
             self.success_url = 'dbs:Criminal-Record-Check-Summary-View'
         else:
-            raise ValueError('The field "has_convictions" is not equal to either True or False.')
+            raise ValueError('The field "has_convictions" is not equal to either "True" or "False".')
 
         criminal_checks_record['dbs_number'] = dbs_number
         criminal_checks_record['has_convictions'] = has_convictions
