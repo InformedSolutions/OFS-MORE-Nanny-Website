60f5a85e3e89c44d43231f457c33c66be731fba1
proctorj <james.proctor@informed.com>
2018-10-25 09:39:24 +0100

    Changed 'children not living with applicant' logic so that children living with a differnt address are considered not just those without an address. Minor updates to comments and formatting

--- your_children_app/views/your_children_addresses.py
+++ your_children_app/views/your_children_addresses.py
@@ -2,9 +2,9 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from nanny.utilities import build_url, app_id_finder
+from nanny.utilities import app_id_finder
 from nanny import NannyGatewayActions
-from nanny.base_views import NannyTemplateView, NannyFormView
+from nanny.base_views import NannyFormView
 from your_children_app.forms.your_children_addresses import YourChildrenLivingWithYouForm
 
 
@@ -16,10 +16,6 @@ class YourChildrenAddressesView(NannyFormView):
     def get(self, request, *args, **kwargs):
         """
         Method for handling GET requests to the 'your children addresses' page
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
         """
         application_id = app_id_finder(self.request)
         form = YourChildrenLivingWithYouForm(id=application_id)
@@ -34,33 +30,28 @@ class YourChildrenAddressesView(NannyFormView):
     def post(self, request, *args, **kwargs):
         """
         Method for handling a POST request from the 'your children addresses' page
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
         """
         application_id = app_id_finder(self.request)
         form = YourChildrenLivingWithYouForm(request.POST, id=application_id)
 
-        # Form does not pass validation
+        # Form does not pass validation, present the page with validation errors
         if not form.is_valid():
-            variables={
+            variables = {
                 'form': form,
                 'application_id': application_id,
             }
 
             return render(request, "your-children-addresses.html", variables)
 
-        # Children selected in the list should be assigned the applicants address
         api_response = NannyGatewayActions().list(
             'your-children', params={'application_id': application_id, 'ordering': 'date_created'}
         )
 
-
+        # Define a list of children from the API response, ordered by the date they were created by the applicant
         children = api_response.record
 
         for child in children:
-            # Append True or False to the child's live's with applicant status
+            # Add True or False to the child's 'lives_with_applicant' status
             child['lives_with_applicant'] = str(child['child']) in form.cleaned_data[
                 'children_living_with_applicant_selection']
 
@@ -87,16 +78,17 @@ class YourChildrenAddressesView(NannyFormView):
                     raise ValueError('The API did not respond as expected')
 
         # Create a list of children who do not live with the applicant
-        children_not_living_with_applicant = [child for child in children if child['street_line1'] is None]
+        children_not_living_with_applicant = [child for child in children if child['lives_with_applicant'] is False]
 
         # If any children do not live with the applicant, the child address sub-task must be presented
         if len(children_not_living_with_applicant) > 0:
+            # Child number is defined by the first child added by the applicant as it is ordered by date created
             child_number = children_not_living_with_applicant[0]['child']
 
             return HttpResponseRedirect(reverse('your-children:Your-Children-address') + '?id=' +
                                         application_id + '&child=' + str(child_number))
 
-        # If all the children live with the applicant, return the summary table and skip the address selction
+        # If all the children live with the applicant, return the summary table and skip the address selection
         else:
             return HttpResponseRedirect(reverse('your-children:Your-Children-Summary') + '?id=' +
                                         application_id)
