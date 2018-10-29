1a9a29c9f17575f89545f2eecbd3ea000f6f0fd2
proctorj <james.proctor@informed.com>
2018-10-29 16:03:46 +0000

    Added forms for address lookup and entry within the 'Your children' task

---
+++ your_children_app/forms/your_children_address.py
@@ -0,0 +1,117 @@
+import re
+
+from django import forms
+from django.conf import settings
+
+from nanny import NannyForm
+from nanny.db_gateways import NannyGatewayActions
+from nanny.forms_helper import full_stop_stripper
+
+
+class YourChildrenAddressForm(NannyForm):
+    """
+    Form that presents a postcode lookup field within the 'Your children' task
+    """
+    field_label_classes = 'form-label-bold'
+    error_summary_template_name = 'standard-error-summary.html'
+    error_summary_title = 'There was a problem'
+    auto_replace_widgets = True
+
+    postcode = forms.CharField(label='Postcode', error_messages={'required': 'Please enter your postcode'})
+
+    def __init__(self, *args, **kwargs):
+        """
+        Method to configure the initialisation of the 'Your Children' address postcode search
+        :param args: arguments passed to the form
+        :param kwargs: keyword arguments passed to the form, e.g. application ID
+        """
+        self.application_id_local = kwargs.pop('id')
+        self.child = kwargs.pop('child')
+        super(YourChildrenAddressForm, self).__init__(*args, **kwargs)
+        full_stop_stripper(self)
+
+        # Set form initial postcode if one exists for the child number
+        self.fields['postcode'].initial = NannyGatewayActions().list('your-children', params={
+            'application_id': self.application_id_local,
+            'child': str(self.child),
+            }).record[0]['postcode']
+
+    def clean_postcode(self):
+        """
+        Postcode validation
+        :return: string containing cleaned postcode
+        """
+        postcode = self.cleaned_data['postcode']
+        postcode_no_space = postcode.replace(" ", "")
+        postcode_uppercase = postcode_no_space.upper()
+        if re.match(settings.REGEX['POSTCODE_UPPERCASE'], postcode_uppercase) is None:
+            raise forms.ValidationError('Please enter a valid postcode')
+        return postcode
+
+
+class YourChildrenAddressSelectionForm(NannyForm):
+    """
+    Form that presents the results of a postcode lookup search
+    """
+    field_label_classes = 'form-label-bold'
+    error_summary_template_name = 'standard-error-summary.html'
+    auto_replace_widgets = True
+
+    address = forms.ChoiceField(label='Select address', required=True,
+                                error_messages={'required': 'Please select your address'})
+
+    def __init__(self, *args, **kwargs):
+        """
+        Method to configure the initialisation of the Your personal details: home address form for postcode search
+        :param args: arguments passed to the form
+        :param kwargs: keyword arguments passed to the form, e.g. application ID
+        """
+        self.application_id_local = kwargs.pop('id')
+        self.choices = kwargs.pop('choices')
+        super(YourChildrenAddressSelectionForm, self).__init__(*args, **kwargs)
+        full_stop_stripper(self)
+        self.fields['address'].choices = self.choices
+
+
+class YourChildrenManualAddressForm(NannyForm):
+    """
+    Form that presents the manual address entry for the 'your children' task
+    """
+    field_label_classes = 'form-label-bold'
+    error_summary_template_name = 'standard-error-summary.html'
+    auto_replace_widgets = True
+
+    street_line1 = forms.CharField(label='Address line 1', required=True, error_messages={
+        'required': 'Please enter the first line of your address'})
+    street_line2 = forms.CharField(label='Address line 2', required=False)
+    town = forms.CharField(label='Town or city', required=True,
+                           error_messages={'required': 'Please enter the name of the town or city'})
+    county = forms.CharField(label='County (optional)', required=False)
+    postcode = forms.CharField(label='Postcode', required=True,
+                               error_messages={'required': 'Please enter your postcode'})
+
+    def __init__(self, *args, **kwargs):
+        """
+        Method to configure the initialisation of the 'Your Children' address manual entry form
+        :param args: arguments passed to the form
+        :param kwargs: keyword arguments passed to the form, e.g. application ID
+        """
+        self.application_id_local = kwargs.pop('id')
+        self.child = kwargs.pop('child')
+        super(YourChildrenManualAddressForm, self).__init__(*args, **kwargs)
+        full_stop_stripper(self)
+
+        # Form initialisation
+        child_record = NannyGatewayActions().list('your-children', params={
+            'application_id': self.application_id_local,
+            'child': str(self.child),
+            }).record[0]
+
+        self.fields['street_line1'].initial = child_record['street_line1']
+        self.fields['street_line2'].initial = child_record['street_line2']
+        self.fields['town'].initial = child_record['town']
+        self.fields['county'].initial = child_record['county']
+        self.fields['postcode'].initial = child_record['postcode']
+        self.pk = child_record['child_id']
+        self.field_list = ['street_line1', 'street_line2', 'town', 'county', 'postcode']
+
--- your_children_app/views/your_children_address.py
+++ your_children_app/views/your_children_address.py
@@ -1,37 +1,96 @@
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
 from nanny.utilities import build_url, app_id_finder
-from nanny import NannyGatewayActions
-from nanny.base_views import NannyTemplateView
+from nanny import NannyGatewayActions, reverse
+from nanny.base_views import NannyFormView
+from your_children_app.forms.your_children_address import YourChildrenAddressForm
 
 
-class YourChildrenAddressView(NannyTemplateView):
+class YourChildrenPostcodeView(NannyFormView):
     """
     Template view to  render the your children postcode lookup view
     """
-    template_name = "your-children-address.html"
-    success_url_name = 'your-children:Your-Children-address-lookup'
 
-    def post(self, request):
+    def get(self, request, *args, **kwargs):
+        """
+        Method to handle get requests to the 'Your Children' postcode lookup page
+        """
+        application_id = request.GET["id"]
+        child = request.GET["child"]
+        form = YourChildrenAddressForm(id=application_id, child=child)
+        child_record = NannyGatewayActions().list('your-children', params={
+            'application_id': application_id,
+            'child': str(child),
+        }).record[0]
 
-        app_id = app_id_finder(self.request)
-        app_api_response = NannyGatewayActions().read('application', params={'application_id': app_id})
-        if app_api_response.status_code == 200:
-            record = app_api_response.record
-            record['your_children_status'] = 'IN_PROGRESS'
-            NannyGatewayActions().put('application', params=record)
+        name = child_record['first_name'] + " " + child_record['last_name']
+        variables = {
+            'form': form,
+            'name': name,
+            'application_id': application_id,
+            'child': child,
+        }
 
-        return HttpResponseRedirect(build_url('your-children:Your-Children-address-lookup', get={'id': app_id}))
+        return render(request, 'your-children-address.html', variables)
+
+    def post(self, request, *args, **kwargs):
+        """
+        Method to handle post requests from the 'Your Children' postcode lookup page
+        """
+        application_id = request.POST["id"]
+        child = request.POST["child"]
+        form = YourChildrenAddressForm(id=application_id, child=child)
+        child_record = NannyGatewayActions().list('your-children', params={
+            'application_id': application_id,
+            'child': str(child),
+        }).record[0]
+
+        application_api = NannyGatewayActions().read('application', params={'application_id': application_id})
+
+        if 'postcode-search' in request.POST:
+
+            if form.is_valid():
+                # Update child record
+                postcode = form.cleaned_data.get('postcode')
+                child_record['postcode'] = postcode
+                NannyGatewayActions().patch('your-children', params=child_record)
+
+                # Update task status
+                if application_api['application_status'] != 'COMPLETED':
+                    application_api['application_status'] = 'IN_PROGRESS'
+
+                return HttpResponseRedirect(reverse('your-children:Your-Children-address-lookup')
+                                            + '?id=' + application_id + '&child=' + str(child))
+
+            else:
+                # Form is not valid
+                form.error_summary_title = 'There was a problem with your postcode'
+
+                if application_api['application_status'] == 'FURTHER_INFORMATION':
+                    form.error_summary_template_name = 'returned-error-summary.html'
+                    form.error_summary_title = 'There was a problem'
+
+                name = child_record['first_name'] + " " + child_record['last_name']
+
+                variables = {
+                    'form': form,
+                    'name': name,
+                    'application_id': application_id,
+                    'child': child,
+                }
+
+                return render(request, 'your-children-address-lookup.html', variables)
 
 
-class YourChildrenAddressLookupView(NannyTemplateView):
+class YourChildrenAddressSelectionView(NannyFormView):
     """
     Template view to  render the your children address selection view
     """
     template_name = "your-children-address-lookup.html"
     success_url_name = 'your-children:Your-Children-Summary'
 
-    def post(self, request):
-
+    def post(self, request, *args, **kwargs):
         app_id = app_id_finder(self.request)
         app_api_response = NannyGatewayActions().read('application', params={'application_id': app_id})
         if app_api_response.status_code == 200:
@@ -42,15 +101,14 @@ class YourChildrenAddressLookupView(NannyTemplateView):
         return HttpResponseRedirect(build_url('your-children:Your-Children-Summary', get={'id': app_id}))
 
 
-class YourChildrenManualAddressView(NannyTemplateView):
+class YourChildrenManualAddressView(NannyFormView):
     """
     Template view to  render the your children details view
     """
     template_name = "your-children-address-manual.html"
     success_url_name = 'your-children:Your-Children-Summary'
 
-    def post(self, request):
-
+    def post(self, request, *args, **kwargs):
         app_id = app_id_finder(self.request)
         app_api_response = NannyGatewayActions().read('application', params={'application_id': app_id})
         if app_api_response.status_code == 200:
