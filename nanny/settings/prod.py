cd231e966f6428f2650d8b3de73a57343f07c08c
LRydeard <luke.rydeard@informed.com>
2018-10-30 09:22:55 +0000

    Removed secret key from prod settings and set ALLOWED_HOSTS to all

--- nanny/settings/prod.py
+++ nanny/settings/prod.py
@@ -1,5 +1,6 @@
 from .base import *
 
+ALLOWED_HOSTS = ['*']
 
 DEBUG = False
 
@@ -8,4 +9,4 @@ PROD_APPS = []
 INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROD_APPS + PROJECT_APPS
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', ')8a7c-wwy%xm=u2n#(whzdile^imbt)q89s+9!5d(ci=p=l+mm')
+SECRET_KEY = os.environ.get('SECRET_KEY')
