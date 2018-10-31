f96419c1ec0b0a27bb12ffb44e3ad3e0b72dfe1d
proctorj <proctorj@informed.com>
2018-10-31 17:02:04 +0000

    Created tests for 'Your children' app - Currently 4 errors and no failures

---
+++ your_children_app/tests/test_views/test_your_children_addresses.py
@@ -0,0 +1,43 @@
+from ..tests import authenticate, YourChildrenTests
+from django.urls import resolve, reverse
+from unittest import mock
+from ...views import *
+import uuid
+from django.template.response import TemplateResponse
+
+from nanny.test_utils import side_effect
+
+
+@mock.patch("nanny.db_gateways.IdentityGatewayActions.read", authenticate)
+class Addresses(YourChildrenTests):
+
+    def test_name_resolves_to_page(self):
+        found = resolve(reverse('your-children:Your-Children-addresses'))
+        self.assertCountEqual(found.func.__name__, YourChildrenAddressesView.__name__)
+
+    def test_can_render_addresses_page(self):
+        with mock.patch('nanny.db_gateways.NannyGatewayActions.read') as nanny_api_read, \
+                mock.patch('nanny.db_gateways.NannyGatewayActions.put') as nanny_api_put, \
+                mock.patch('nanny.db_gateways.NannyGatewayActions.list'):
+            nanny_api_read.side_effect = side_effect
+            nanny_api_put.side_effect = side_effect
+
+            response = self.client.get(build_url('your-children:Your-Children-addresses', get={
+                'id': uuid.UUID
+            }))
+
+            self.assertEqual(response.status_code, 200)
+
+    def test_can_submit_valid_addresses_page(self):
+        with mock.patch('nanny.db_gateways.NannyGatewayActions.read') as nanny_api_read, \
+                mock.patch('nanny.db_gateways.NannyGatewayActions.put') as nanny_api_put, \
+                mock.patch('nanny.db_gateways.NannyGatewayActions.list'):
+            nanny_api_read.side_effect = side_effect
+            nanny_api_put.side_effect = side_effect
+
+            response = self.client.post(build_url('your-children:Your-Children-addresses', get={
+                'id': uuid.uuid4()
+            }), )
+
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue('your-children/address/' in response.url)
