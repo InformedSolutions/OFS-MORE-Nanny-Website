9bde258d7e8665c50be947ab33f2f444d5ea894d
Thomas-95IS <thomas.stokes@informed.com>
2019-01-11 14:06:44 +0000

    Use coverage configurations in place of shell scripts.

--- testcoverage.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Script to test unit test coverage for app-childminder and produce html document from report
-#. ./dev-env-vars.txt
-rm -r htmlcov 2> /dev/null
-coverage erase
-coverage run --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/*,*tests/*,*settings/* --branch /vagrant/OFS-MORE-DevOps-Tooling/app-nanny/manage.py test application --settings=nanny.settings.dev --exclude-tag=selenium #--no-input
-coverage report -m --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/*,*tests/*,*settings/*
-coverage html --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/*,*tests/*,*settings/* -d htmlcov
-firefox htmlcov/index.html
