8faa2680c73747312b1a5543f386a0ff882fd775
jenniferbrown94 <jennifer.brown@informed.com>
2019-02-14 14:29:11 +0000

    type of check form

--- application/presentation/dbs/forms/dbs_type.py
+++ application/presentation/dbs/forms/dbs_type.py
@@ -3,6 +3,7 @@ from django import forms
 from govuk_forms.widgets import InlineRadioSelect
 
 from application.presentation.utilities import NannyForm
+from application.services.ConditionalPostChoiceWidget import ConditionalPostInlineRadioSelect
 
 
 class DBSTypeForm(NannyForm):
@@ -12,16 +13,38 @@ class DBSTypeForm(NannyForm):
     field_label_classes = 'form-label-bold'
     error_summary_title = 'There was a problem'
     auto_replace_widgets = True
+    capita_field_name = 'is_ofsted_dbs'
+    update_field_name = 'on_dbs_update_service'
 
     options = (
         ('True', 'Yes'),
         ('False', 'No')
     )
 
-    is_ofsted_dbs = forms.ChoiceField(
-            label='Do you have an Ofsted DBS check?',
-            choices=options,
-            widget=InlineRadioSelect,
-            required=True,
-            error_messages={'required': 'Please say if you have an Ofsted DBS check'}
+    on_dbs_update_service = forms.ChoiceField(
+        label=' Are you on the DBS update service?',
+        choices=options,
+        widget=InlineRadioSelect,
+        required=True,
+        error_messages={'required': 'Please say if you are on the DBS Update Service'}
     )
+
+    def __init__(self, *args, **kwargs):
+        ask_if_capita = kwargs['initial']['is_ofsted_dbs']
+        if not ask_if_capita:
+            self.reveal_conditionally = {'is_ofsted_dbs': {'True': self.update_field_name}}
+        super().__init__(*args, **kwargs)
+        if not ask_if_capita:
+            self.fields[self.capita_field_name] = self.get_ofsted_dbs_field_data()
+
+
+    def get_ofsted_dbs_field_data(self):
+        return forms.ChoiceField(
+            label='Is it an enhanced check for home-based childcare?',
+            choices=self.options,
+            widget=ConditionalPostInlineRadioSelect,
+            required=True,
+            error_messages={'required': 'Please say if you have an enhanced check for home-based childcare'}
+        )
+
+
