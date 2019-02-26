d24da4a1bdbd7d20ac98a9eb9890ed185bd3423a
LRydeard <luke.rydeard@informed.com>
2019-02-26 09:42:08 +0000

    Removed the your_children header being added in the master summary, removed your_children files from the application folder.

--- application/presentation/your_children/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from .your_children_address import *
-from .your_children_addresses import *
-from .your_children_details import *
-from .your_children_summary import *
-from .your_children_guidance import *
