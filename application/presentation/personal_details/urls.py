8d28be2471e431a418e9b0eb5a0dd696a2114395
katej321 <klj525@york.ac.uk>
2019-02-27 12:18:08 +0000

    Removal of 'Lived abroad' question in personal details

--- application/presentation/personal_details/urls.py
+++ application/presentation/personal_details/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 
 from .views import PersonalDetailNameView, PersonalDetailDOBView, PersonalDetailHomeAddressView, \
     PersonalDetailSelectAddressView, PersonalDetailManualAddressView, PersonalDetailSummaryAddressView, \
-    PersonalDetailLivedAbroadView, PersonalDetailCertificateView, PersonalDetailsPostCertificateView, \
     PersonalDetailsYourChildrenView, Summary
 
 urlpatterns = [
@@ -12,10 +11,6 @@ urlpatterns = [
     url(r'^personal-details/select-home-address/', PersonalDetailSelectAddressView.as_view(), name='Personal-Details-Select-Address'),
     url(r'^personal-details/enter-home-address/', PersonalDetailManualAddressView.as_view(), name='Personal-Details-Manual-Address'),
     url(r'^personal-details/home-address-details/', PersonalDetailSummaryAddressView.as_view(), name='Personal-Details-Address-Summary'),
-    url(r'^personal-details/lived-abroad/', PersonalDetailLivedAbroadView.as_view(), name='Personal-Details-Lived-Abroad'),
-    url(r'^personal-details/good-conduct-certificates/', PersonalDetailCertificateView.as_view(),
-        name='Personal-Details-Certificates-Of-Good-Conduct'),
-    url(r'^personal-details/post-certificates/', PersonalDetailsPostCertificateView.as_view(), name='Personal-Details-Post-Certificates'),
     url(r'^personal-details/your-children/', PersonalDetailsYourChildrenView.as_view(), name='Personal-Details-Your-Children'),
     url(r'^personal-details/check-answers/', Summary.as_view(), name='Personal-Details-Summary')
 ]
