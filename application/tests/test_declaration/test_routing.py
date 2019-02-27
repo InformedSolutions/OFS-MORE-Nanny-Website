88f99335801c529958df9a4e3d3107d66ee9a906
LRydeard <luke.rydeard@informed.com>
2019-02-27 14:40:46 +0000

    Re-enabled tests that were previously commented out, in-progress on fixing these tests

--- application/tests/test_declaration/test_routing.py
+++ application/tests/test_declaration/test_routing.py
@@ -1,22 +1,20 @@
+from http.cookies import SimpleCookie
 from unittest import mock
 
-from http.cookies import SimpleCookie
-
-from django.test import TestCase, modify_settings
+from django.test import modify_settings, TestCase
 from django.urls import resolve, reverse
 
 from application.presentation.declaration import views
-
-from application.tests.test_utils  import side_effect
 from application.services.db_gateways import IdentityGatewayActions, NannyGatewayActions
+from application.tests.test_utils import side_effect
 
 
 @modify_settings(MIDDLEWARE={
-        'remove': [
-            'middleware.CustomAuthenticationHandler',
-        ]
-    })
-class DeclarationTests(TestCase):
+    'remove': [
+        'middleware.CustomAuthenticationHandler',
+    ]
+})
+class DeclarationRoutingTests(TestCase):
 
     def setUp(self):
         self.application_id = 'ef78049d-40fb-4808-943c-593fa3a9700b'
@@ -34,54 +32,55 @@ class DeclarationTests(TestCase):
         self.application_record = {
             'application_id': self.application_id,
         }
-        self.user_details_record ={
+        self.user_details_record = {
             'email': 'knights@ni.com'
         }
 
-    # def test_no_link_to_master_summay_page_without_completed_tasks(self):
-    #     """
-    #     Test to assert that the 'Childcare-Training-Guidance' page can be rendered.
-    #     """
-    #     with mock.patch('nanny_models.childcare_training.ChildcareTraining.api.get_record') as nanny_api_get, \
-    #         mock.patch('nanny_models.childcare_training.ChildcareTraining.api.get_record') as identity_api_get:
-    #
-    #         for task in self.tasks:
-    #             self.application_record[task] = 'NOT_STARTED'
-    #
-    #         nanny_api_get.return_value.record = self.application_record
-    #
-    #         response = self.client.get(reverse('Task-List') + '?id=' + self.application_id)
-    #         link = reverse('Declaration-Summary') + '?id=' + self.application_id
-    #
-    #         self.assertNotIn(link, response.body)
+    def test_no_link_to_master_summary_page_without_completed_tasks(self):
+        """
+        Test to assert that the 'Childcare-Training-Guidance' page can be rendered.
+        """
+        with mock.patch.object(NannyGatewayActions, 'read') as nanny_api_read, \
+                mock.patch.object(IdentityGatewayActions, 'read') as identity_api_read:
+            identity_api_read.side_effect = side_effect
 
-    # def test_link_to_master_summary_page_when_completed_tasks(self):
-    #     """
-    #     Test to assert that the 'Declaration-Summary' page can be rendered if all the tasks are 'COMPLETED'.
-    #     """
-    #     with mock.patch('nanny.db_gateways.NannyGatewayActions.read') as nanny_api_read, \
-    #         mock.patch('nanny.db_gateways.IdentityGatewayActions.read') as identity_api_read:
-    #         nanny_api_read.side_effect = side_effect
-    #         identity_api_read.side_effect = side_effect
-    #
-    #         for task in self.tasks:
-    #             self.application_record[task] = 'COMPLETED'
-    #
-    #         response = self.client.get(reverse('Task-List') + '?id=' + 'a4e6633f-5339-4de5-ae03-69c71fd008b3')
-    #
-    #         self.assertContains(
-    #             response,
-    #             '<a href="{}">'.format(
-    #                 reverse('declaration:Master-Summary') + '?id=' + 'a4e6633f-5339-4de5-ae03-69c71fd008b3'),
-    #             html=True
-    #         )
+            for task in self.tasks:
+                self.application_record[task] = 'NOT_STARTED'
+
+            nanny_api_read.return_value.record = self.application_record
+
+            response = self.client.get(reverse('Task-List') + '?id=' + self.application_id)
+            link = reverse('Declaration-Summary') + '?id=' + self.application_id
+
+            self.assertNotIn(link, response.body)
+
+    def test_link_to_master_summary_page_when_completed_tasks(self):
+        """
+        Test to assert that the 'Declaration-Summary' page can be rendered if all the tasks are 'COMPLETED'.
+        """
+        with mock.patch.object(NannyGatewayActions, 'read') as nanny_api_read, \
+                mock.patch.object(IdentityGatewayActions, 'read') as identity_api_read:
+            nanny_api_read.side_effect = side_effect
+            identity_api_read.side_effect = side_effect
+
+            for task in self.tasks:
+                self.application_record[task] = 'COMPLETED'
+
+            response = self.client.get(reverse('Task-List') + '?id=' + 'a4e6633f-5339-4de5-ae03-69c71fd008b3')
+
+            self.assertContains(
+                response,
+                '<a href="{}">'.format(
+                    reverse('declaration:Master-Summary') + '?id=' + 'a4e6633f-5339-4de5-ae03-69c71fd008b3'),
+                html=True
+            )
 
     def test_can_render_master_summary_page(self):
         """
         Test to assert that the 'Master-Summary' page can be rendered.
         """
         with mock.patch.object(NannyGatewayActions, 'read') as nanny_api_read, \
-            mock.patch.object(IdentityGatewayActions, 'read') as identity_api_read:
+                mock.patch.object(IdentityGatewayActions, 'read') as identity_api_read:
             nanny_api_read.side_effect = side_effect
             identity_api_read.side_effect = side_effect
 
@@ -109,7 +108,7 @@ class DeclarationTests(TestCase):
         Test to assert that the 'Master-Summary' page can be rendered.
         """
         with mock.patch.object(NannyGatewayActions, 'read') as nanny_api_read, \
-            mock.patch.object(IdentityGatewayActions, 'read') as identity_api_read:
+                mock.patch.object(IdentityGatewayActions, 'read') as identity_api_read:
             nanny_api_read.side_effect = side_effect
             identity_api_read.side_effect = side_effect
 
@@ -123,8 +122,8 @@ class DeclarationTests(TestCase):
         """
         Test to assert that the 'Final-Declaration' page can be rendered.
         """
-        with mock.patch.object(NannyGatewayActions,'read') as nanny_api_read, \
-            mock.patch.object(IdentityGatewayActions, 'read') as identity_api_read:
+        with mock.patch.object(NannyGatewayActions, 'read') as nanny_api_read, \
+                mock.patch.object(IdentityGatewayActions, 'read') as identity_api_read:
             nanny_api_read.side_effect = side_effect
             identity_api_read.side_effect = side_effect
 
@@ -140,7 +139,7 @@ class DeclarationTests(TestCase):
         'Payment-Details' page.
         """
         with mock.patch.object(NannyGatewayActions, 'read') as nanny_api_read, \
-            mock.patch.object(IdentityGatewayActions, 'read') as identity_api_read:
+                mock.patch.object(IdentityGatewayActions, 'read') as identity_api_read:
             nanny_api_read.side_effect = side_effect
             identity_api_read.side_effect = side_effect
 
@@ -155,9 +154,3 @@ class DeclarationTests(TestCase):
 
             self.assertEqual(response.status_code, 302)
             # self.assertEqual(found.func.view_class, views.PaymentDetails)
-
-    def test_unselected_declaration_boxes_raise_form_error(self):
-        """
-        Test that not selecting each declaration box raises the respective error.
-        """
-        pass
