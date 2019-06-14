68ac068180ad684dd242b7e047f69683674c87f1
jenniferbrown94 <44293392+jenniferbrown94@users.noreply.github.com>
2019-06-14 12:10:45 +0100

    Update prod.py

--- nanny/settings/prod.py
+++ nanny/settings/prod.py
@@ -10,4 +10,4 @@ INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROD_APPS + PROJECT_APPS
 FEEDBACK_EMAIL = 'registrationpilot@ofsted.gov.uk'
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', ')8a7c-wwy%xm=u2n#(whzdile^imbt)q89s+9!5d(ci=p=l+mm')
+SECRET_KEY = os.environ.get('SECRET_KEY', ')
