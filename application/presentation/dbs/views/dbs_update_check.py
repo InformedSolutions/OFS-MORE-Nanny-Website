6eb5e6eefed0b7a875047634ca1cb5d8478cfee4
jenniferbrown94 <jennifer.brown@informed.com>
2019-11-19 15:50:36 +0000

    add comments

--- application/presentation/dbs/views/dbs_update_check.py
+++ application/presentation/dbs/views/dbs_update_check.py
@@ -14,8 +14,8 @@ class DBSUpdateCheckView(NannyTemplateView):
 
     def get_context_data(self, **kwargs):
         """
-        On a post request, set the task status to completed and redirect the user to the task list
-        :return:
+        Get the context data and redirect url
+        :return: context
         """
         post_dbs, summary = self.success_url_name
         application_id = app_id_finder(self.request)
@@ -23,8 +23,10 @@ class DBSUpdateCheckView(NannyTemplateView):
         if crc_response.status_code == 200:
             capita = crc_response.record['is_ofsted_dbs']
             no_information = crc_response.record['certificate_information'] in NO_ADDITIONAL_CERTIFICATE_INFORMATION
+            # if the dbs is on capita and has no info redirect to the summary
             if capita and no_information:
                 self.success_url_name = summary
+            # otherwise redirect to the post dbs page
             else:
                 self.success_url_name = post_dbs
         else:
