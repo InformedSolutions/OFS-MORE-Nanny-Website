c8baf1081c5a986247846a54c344e0146afc947e
jenniferbrown94 <jennifer.brown@informed.com>
2019-11-19 11:58:58 +0000

    update check page only goes to post dbs if not capita or info

--- application/presentation/dbs/views/dbs_update_check.py
+++ application/presentation/dbs/views/dbs_update_check.py
@@ -1,4 +1,8 @@
 from application.presentation.base_views import NannyTemplateView
+from django.http import HttpResponseRedirect
+
+from ...utilities import app_id_finder, build_url, NO_ADDITIONAL_CERTIFICATE_INFORMATION
+from application.services.db_gateways import NannyGatewayActions
 
 
 class DBSUpdateCheckView(NannyTemplateView):
@@ -6,4 +10,25 @@ class DBSUpdateCheckView(NannyTemplateView):
     Template view to render the DBS update-check page.
     """
     template_name = 'dbs-update-check.html'
-    success_url_name = 'dbs:Post-DBS-Certificate'
+    success_url_name = ('dbs:Post-DBS-Certificate', 'dbs:Criminal-Record-Check-Summary-View')
+
+    def get_context_data(self, **kwargs):
+        """
+        On a post request, set the task status to completed and redirect the user to the task list
+        :return:
+        """
+git         post_dbs, summary = self.success_url_name
+        application_id = app_id_finder(self.request)
+        crc_response = NannyGatewayActions().read('dbs-check', params={'application_id': application_id})
+        if crc_response.status_code == 200:
+            capita = crc_response.record['is_ofsted_dbs']
+            no_information = crc_response.record['certificate_information'] in NO_ADDITIONAL_CERTIFICATE_INFORMATION
+            if capita and no_information:
+                self.success_url_name = summary
+            else:
+                self.success_url_name = post_dbs
+        else:
+            self.success_url_name = summary
+
+        context = super(DBSUpdateCheckView, self).get_context_data(**kwargs)
+        return context
